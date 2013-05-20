09bd227fe8a511a21be602183151ccce4a9aa05f
Matthew Wedgwood <matthew.wedgwood@canonical.com>
2013-05-20 18:00:09 -0500

    Include Hooks class in hookenv for concise hooks setup in charms

--- charmhelpers/core/hookenv.py
+++ charmhelpers/core/hookenv.py
@@ -210,3 +210,32 @@ def close_port(port, protocol="TCP"):
 def unit_get(attribute):
     _args = ['unit-get', attribute]
     return subprocess.check_output(_args).strip()
+
+
+class UnregisteredHookError(Exception):
+    pass
+
+
+class Hooks(object):
+    def __init__(self):
+        super(Hooks, self).__init__()
+        self._hooks = {}
+
+    def register(self, name, function):
+        self._hooks[name] = function
+
+    def execute(self, args):
+        hook_name = os.path.basename(args[0])
+        if hook_name in self._hooks:
+            self._hooks[hook_name]()
+        else:
+            raise UnregisteredHookError(hook_name)
+
+    def hook(self, *hook_names):
+        def wrapper(decorated):
+            for hook_name in hook_names:
+                self.register(hook_name, decorated)
+            else:
+                self.register(decorated.__name__, decorated)
+            return decorated
+        return wrapper
