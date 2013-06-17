2eb928a68a5a7708ce7d97529b211f0bb7606061
Adam Gandelman <adamg@canonical.com>
2013-06-17 15:12:43 -0700

    contrib.openstack.context: Update keystone context to act on all relations+units.

--- charmhelpers/contrib/openstack/context.py
+++ charmhelpers/contrib/openstack/context.py
@@ -10,17 +10,20 @@ from charmhelpers.core.hookenv import (
 class OSContextError(Exception):
     pass
 
+
 def context_complete(ctxt):
     _missing = []
     for k, v in ctxt.iteritems():
-        if v == None or v == '':
+        if v is None or v == '':
             _missing.append(k)
     if _missing:
         print 'Missing required data: %s' % ' '.join(_missing)
         return False
     return True
 
+
 def shared_db(relation_id=None, unit_id=None):
+    log('Generating template context for shared-db')
     conf = config()
     try:
         database = conf['database']
@@ -36,30 +39,44 @@ def shared_db(relation_id=None, unit_id=None):
                 'database_host': relation_get('db_host', rid=rid, unit=unit),
                 'database': database,
                 'database_user': username,
-                'database_password': relation_get('password', rid=rid, unit=unit)
+                'database_password': relation_get('password', rid=rid,
+                                                  unit=unit)
             }
     if not context_complete(ctxt):
         return {}
     return ctxt
 
+
 def identity_service(relation_id=None):
-    ctxt = {
-        'service_port': relation_get('service_port'),
-        'service_host': relation_get('service_host'),
-        'auth_host': relation_get('auth_host'),
-        'auth_port': relation_get('auth_port'),
-        'admin_tenant_name': relation_get('service_tenant'),
-        'admin_user': relation_get('service_username'),
-        'admin_password': relation_get('service_password'),
-        # XXX: Hard-coded http.
-        'service_protocol': 'http',
-        'auth_protocol': 'http',
-    }
+    log('Generating template context for identity-service')
+    ctxt = {}
+
+    for rid in relation_ids('identity-service'):
+        for unit in related_units(rid):
+            ctxt = {
+                'service_port': relation_get('service_port', rid=rid,
+                                             unit=unit),
+                'service_host': relation_get('service_host', rid=rid,
+                                             unit=unit),
+                'auth_host': relation_get('auth_host', rid=rid, unit=unit),
+                'auth_port': relation_get('auth_port', rid=rid, unit=unit),
+                'admin_tenant_name': relation_get('service_tenant', rid=rid,
+                                                  unit=unit),
+                'admin_user': relation_get('service_username', rid=rid,
+                                           unit=unit),
+                'admin_password': relation_get('service_password', rid=rid,
+                                               unit=unit),
+                # XXX: Hard-coded http.
+                'service_protocol':  'http',
+                'auth_protocol':  'http',
+            }
     if not context_complete(ctxt):
         return {}
     return ctxt
 
+
 def amqp(relation_id=None):
+    log('Generating template context for amqp')
     conf = config()
     try:
         username = conf['rabbit-user']
@@ -81,7 +98,7 @@ def amqp(relation_id=None):
                 'rabbitmq_host': rabbitmq_host,
                 'rabbitmq_user': username,
                 'rabbitmq_password': relation_get('password', rid=rid,
-                                                   unit=unit),
+                                                  unit=unit),
                 'rabbitmq_virtual_host': vhost,
             }
     if not context_complete(ctxt):
