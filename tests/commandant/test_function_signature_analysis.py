75c41d4481f3f775218b3dc4d1f6838889e88556
Nick Moffitt <nick.moffitt@canonical.com>
2013-06-27 14:32:50 +0100

    fix tests

--- tests/commandant/test_function_signature_analysis.py
+++ tests/commandant/test_function_signature_analysis.py
@@ -3,20 +3,20 @@ determine the parameters to argparse."""
 
 from testtools import TestCase, matchers
 
-from charmhelpers.commandant import signature
+from charmhelpers import cli
 
 class FunctionSignatureTest(TestCase):
     """Test a variety of function signatures."""
 
     def test_positional_arguments(self):
         """Finite number of order-dependent required arguments."""
-        argparams = signature.describe_arguments(lambda x, y, z: False)
+        argparams = cli.describe_arguments(lambda x, y, z: False)
         self.assertEqual(tuple(argparams),
                 ((('x',), {}), (('y',), {}), (('z',), {})))
 
     def test_keyword_arguments(self):
         """Function has optional parameters with default values."""
-        argparams = tuple(signature.describe_arguments(
+        argparams = tuple(cli.describe_arguments(
             lambda x, y=3, z="bar": False))
         self.assertIn((('y',), {'default': 3}), argparams)
         self.assertIn((('z',), {'default': 'bar'}), argparams)
@@ -24,14 +24,14 @@ class FunctionSignatureTest(TestCase):
     def test_varargs(self):
         """Function has a splat-operator parameter to catch an arbitrary number
         of positional parameters."""
-        argparams = tuple(signature.describe_arguments(
+        argparams = tuple(cli.describe_arguments(
             lambda x, y=3, *z: False))
         self.assertIn((('z',), {'nargs': '*'}), argparams)
 
     def test_keyword_splat_missing(self):
         """Double-splat arguments can't be represented in the current version
         of commandant."""
-        args = signature.describe_arguments(
+        args = cli.describe_arguments(
                 lambda x, y=3, *z, **missing: False)
         for opts, _ in args:
             # opts should be ('varname',) at this point
