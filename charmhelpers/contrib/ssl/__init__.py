dc09504fd292334d4c71296418ee4789f43dc870
Jacek Nykis <jacek.nykis@canonical.com>
2013-08-27 18:08:44 +0100

    Added docstring to the generate_selfsigned function. Improved the function to accept single cn argument and partial subject dict

--- charmhelpers/contrib/ssl/__init__.py
+++ charmhelpers/contrib/ssl/__init__.py
@@ -1,19 +1,73 @@
 import subprocess
+from charmhelpers.core import hookenv
 
 
-def generate_selfsigned(keyfile, certfile, subject=None, config=None):
-    if subject:
-        ssl_subject = "/C={subject[country]}/ST={subject[state]}/L={subject[locality]}" \
-            "/O={subject[organization]}/OU={subject[organizational_unit]}/CN={subject[cn]}" \
-            "/emailAddress={subject[email]}".format(subject=subject)
+def generate_selfsigned(keyfile, certfile, keysize="1024", config=None, subject=None, cn=None):
+    """Generate selfsigned SSL keypair
+
+    You must provide one of the 3 optional arguments:
+    config, subject or cn
+    If more than one is provided the leftmost will be used
+
+    Arguments:
+    keyfile -- (required) full path to the keyfile to be created
+    certfile -- (required) full path to the certfile to be created
+    keysize -- (optional) SSL key length
+    config -- (optional) openssl configuration file
+    subject -- (optional) dictionary with SSL subject variables
+    cn -- (optional) cerfificate common name
+
+    Required keys in subject dict:
+    cn -- Common name (eq. FQDN)
+
+    Optional keys in subject dict
+    country -- Country Name (2 letter code)
+    state -- State or Province Name (full name)
+    locality -- Locality Name (eg, city)
+    organization -- Organization Name (eg, company)
+    organizational_unit -- Organizational Unit Name (eg, section)
+    email -- Email Address
+    """
+
+    cmd = []
+    if config:
         cmd = ["/usr/bin/openssl", "req", "-new", "-newkey",
-               "rsa:1024", "-days", "365", "-nodes", "-x509",
-               "-keyout", keyfile,
-               "-out",    certfile, "-subj", ssl_subject]
-    elif config:
-        cmd = ["/usr/bin/openssl", "req", "-new", "-newkey",
-               "rsa:1024", "-days", "365", "-nodes", "-x509",
+               "rsa:{}".format(keysize), "-days", "365", "-nodes", "-x509",
                "-keyout", keyfile,
                "-out",    certfile, "-config", config]
+    elif subject:
+        ssl_subject = ""
+        if "country" in subject:
+            ssl_subject = ssl_subject + "/C={}".format(subject["country"])
+        if "state" in subject:
+            ssl_subject = ssl_subject + "/ST={}".format(subject["state"])
+        if "locality" in subject:
+            ssl_subject = ssl_subject + "/L={}".format(subject["locality"])
+        if "organization" in subject:
+            ssl_subject = ssl_subject + "/O={}".format(subject["organization"])
+        if "organizational_unit" in subject:
+            ssl_subject = ssl_subject + "/OU={}".format(subject["organizational_unit"])
+        if "cn" in subject:
+            ssl_subject = ssl_subject + "/CN={}".format(subject["cn"])
+        else:
+            hookenv.log("When using \"subject\" argument you must " \
+                "provide \"cn\" field at very least")
+            return False
+        if "email" in subject:
+            ssl_subject = ssl_subject + "/emailAddress={}".format(subject["email"])
 
+        cmd = ["/usr/bin/openssl", "req", "-new", "-newkey",
+               "rsa:{}".format(keysize), "-days", "365", "-nodes", "-x509",
+               "-keyout", keyfile,
+               "-out",    certfile, "-subj", ssl_subject]
+    elif cn:
+        cmd = ["/usr/bin/openssl", "req", "-new", "-newkey",
+               "rsa:{}".format(keysize), "-days", "365", "-nodes", "-x509",
+               "-keyout", keyfile,
+               "-out",    certfile, "-subj", "/CN={}".format(cn)]
+
+    if not cmd:
+        hookenv.log("No config, subject or cn provided," \
+                "unable to generate self signed SSL certificates")
+        return False
     subprocess.check_call(cmd)
--- tests/contrib/ssl/test_ssl.py
+++ tests/contrib/ssl/test_ssl.py
@@ -1,40 +1,9 @@
-from contextlib import contextmanager
 import subprocess
 
 from mock import patch, call, MagicMock
 from testtools import TestCase
 
 from charmhelpers.contrib import ssl
-
-
-@contextmanager
-def patch_open():
-    '''Patch open() to allow mocking both open() itself and the file that is
-    yielded.
-
-    Yields the mock for "open" and "file", respectively.'''
-    mock_open = MagicMock(spec=open)
-    mock_file = MagicMock(spec=file)
-
-    @contextmanager
-    def stub_open(*args, **kwargs):
-        mock_open(*args, **kwargs)
-        yield mock_file
-
-    with patch('__builtin__.open', stub_open):
-        yield mock_open, mock_file
-
-
-@contextmanager
-def mock_open(filename, contents=None):
-    ''' Slightly simpler mock of open to return contents for filename '''
-    def mock_file(*args):
-        if args[0] == filename:
-            return io.StringIO(contents)
-        else:
-            return open(*args)
-    with patch('__builtin__.open', mock_file):
-        yield
 
 
 class HelpersTest(TestCase):
@@ -55,11 +24,21 @@ class HelpersTest(TestCase):
                                       '-nodes', '-x509', '-keyout',
                                       'mykey.key', '-out', 'mycert.crt',
                                       '-subj',
-                                      '/C=UK/ST=my_state/L=my_locality/'
-                                      'O=my_organization/OU=my_unit/'
-                                      'CN=mysite.example.com/'
-                                      'emailAddress=me@example.com']
+                                      '/C=UK/ST=my_state/L=my_locality'
+                                      '/O=my_organization/OU=my_unit'
+                                      '/CN=mysite.example.com'
+                                      '/emailAddress=me@example.com']
                                      )
+
+    @patch('charmhelpers.core.hookenv.log')
+    def test_generate_selfsigned_failure(self, mock_log):
+        # This is NOT enough, functino requires cn key
+        subject = {"country": "UK",
+                   "locality": "my_locality"}
+
+        result = ssl.generate_selfsigned("mykey.key", "mycert.crt", subject=subject)
+        self.assertFalse(result)
+
 
     @patch('subprocess.check_call')
     def test_generate_selfsigned_file(self, mock_call):
@@ -69,3 +48,13 @@ class HelpersTest(TestCase):
                                       '-nodes', '-x509', '-keyout',
                                       'mykey.key', '-out', 'mycert.crt',
                                       '-config', 'test.cnf'])
+
+
+    @patch('subprocess.check_call')
+    def test_generate_selfsigned_cn_key(self, mock_call):
+        ssl.generate_selfsigned("mykey.key", "mycert.crt", keysize="2048", cn="mysite.example.com")
+        mock_call.assert_called_with(['/usr/bin/openssl', 'req', '-new',
+                                      '-newkey', 'rsa:2048', '-days', '365',
+                                      '-nodes', '-x509', '-keyout',
+                                      'mykey.key', '-out', 'mycert.crt',
+                                      '-subj', '/CN=mysite.example.com'])
