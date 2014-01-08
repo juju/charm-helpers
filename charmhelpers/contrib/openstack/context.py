803e418d16ad14bfade29d1164cb8b4cec549ebb
yolanda.robla@canonical.com <>
2014-01-08 12:01:03 +0100

    only sending rabbitmq_hosts when there are more than 1

--- charmhelpers/contrib/openstack/context.py
+++ charmhelpers/contrib/openstack/context.py
@@ -182,7 +182,7 @@ class AMQPContext(OSContextGenerator):
                     # Sufficient information found = break out!
                     break
             # Used for active/active rabbitmq >= grizzly
-            if 'clustered' not in ctxt and len(related_units(rid))>0:
+            if 'clustered' not in ctxt and len(related_units(rid))>1:
                 rabbitmq_hosts = []
                 for unit in related_units(rid):
                     rabbitmq_hosts.append(relation_get('private-address',
