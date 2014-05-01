f0937869703740a425864c1d6adc843e25aa57f8
Jorge Niedbalski <jorge.niedbalski@canonical.com>
2014-05-01 13:43:33 -0400

    Added charmhelpers.contrib.python.debug. 	- Added debug.set_trace(addr, port) for setting a remote python debugger session. 	- Added tests for cover the set_trace method.

---
+++ tests/contrib/python/test_debug.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+__author__ = "Jorge Niedbalski <jorge.niedbalski@canonical.com>"
+
+from unittest import TestCase
+
+from charmhelpers.contrib.python import debug
+
+import mock
+
+TO_PATCH = [
+    "log",
+    "open_port",
+    "close_port",
+    "Rpdb",
+    "_error",
+]
+
+
+class DebugTestCase(TestCase):
+    """Test cases for charmhelpers.contrib.python.debug"""
+
+    def setUp(self):
+        TestCase.setUp(self)
+        self.patch_all()
+        self.log.return_value = True
+        self.close_port.return_value = True
+
+        self.wrapped_function = mock.Mock(return_value=True)
+        self.set_trace = debug.set_trace
+
+    def patch(self, method):
+        _m = mock.patch.object(debug, method)
+        _mock = _m.start()
+        self.addCleanup(_m.stop)
+        return _mock
+
+    def patch_all(self):
+        for method in TO_PATCH:
+            setattr(self, method, self.patch(method))
+
+    def test_debug_set_trace(self):
+        """Check if set_trace works
+        """
+        self.set_trace()
+        self.open_port.assert_called_with(4444)
+
+    def test_debug_set_trace_ex(self):
+        """Check if set_trace raises exception
+        """
+        self.set_trace()
+        self.Rpdb.set_trace.side_effect = Exception()
+        self._error.assert_called_once()
