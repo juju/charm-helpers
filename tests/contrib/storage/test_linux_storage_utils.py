746482ca701a74e5df733d9c8ec0deb9d0b23c00
Christopher Glass <christopher.glass@canonical.com>
2014-05-06 23:25:24 +0200

    Added more tests around the is_block_device function

--- tests/contrib/storage/test_linux_storage_utils.py
+++ tests/contrib/storage/test_linux_storage_utils.py
@@ -24,13 +24,27 @@ class MiscStorageUtilsTests(unittest.TestCase):
         check_call.assert_any_call(['dd', 'if=/dev/zero', 'of=/dev/foo',
                                     'bs=512', 'count=100', 'seek=100'])
 
-    @patch(STORAGE_LINUX_UTILS + '.stat')
     @patch(STORAGE_LINUX_UTILS + '.S_ISBLK')
-    def test_is_block_device(self, s_isblk, stat):
+    @patch('os.path.exists')
+    @patch('os.stat')
+    def test_is_block_device(self, S_ISBLK, exists, stat):
         '''It detects device node is block device'''
-        with patch(STORAGE_LINUX_UTILS + '.S_ISBLK') as isblk:
-            isblk.return_value = True
-            self.assertTrue(storage_utils.is_block_device('/dev/foo'))
+        class fake_stat:
+            st_mode = True
+        S_ISBLK.return_value = fake_stat()
+        exists.return_value = True
+        self.assertTrue(storage_utils.is_block_device('/dev/foo'))
+
+    @patch(STORAGE_LINUX_UTILS + '.S_ISBLK')
+    @patch('os.path.exists')
+    @patch('os.stat')
+    def test_is_block_device_does_not_exist(self, S_ISBLK, exists, stat):
+        '''It detects device node is block device'''
+        class fake_stat:
+            st_mode = True
+        S_ISBLK.return_value = fake_stat()
+        exists.return_value = False
+        self.assertFalse(storage_utils.is_block_device('/dev/foo'))
 
     @patch(STORAGE_LINUX_UTILS + '.check_output')
     def test_is_device_mounted(self, check_output):
