d9709923e1cd35c823f05feb5e43f099ca9333af
james.page@ubuntu.com <>
2014-07-24 09:29:56 +0100

    Rework tests to cover IPv6, update link_local exclusion for ipv6

--- tests/contrib/network/test_ip.py
+++ tests/contrib/network/test_ip.py
@@ -8,6 +8,14 @@ import charmhelpers.contrib.network.ip as net_ip
 from mock import patch
 
 DUMMY_ADDRESSES = {
+    'lo': {
+        17: [{'peer': '00:00:00:00:00:00',
+              'addr': '00:00:00:00:00:00'}],
+        2: [{'peer': '127.0.0.1', 'netmask':
+             '255.0.0.0', 'addr': '127.0.0.1'}],
+        10: [{'netmask': 'ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff',
+              'addr': '::1'}]
+    },
     'eth0': {
         2: [{'addr': '192.168.1.55',
              'broadcast': '192.168.1.255',
@@ -43,31 +51,10 @@ class IPTest(unittest.TestCase):
                                      network, fallback=None, fatal=False):
 
         def side_effect(iface):
-            ffff = 'ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff'
-            results = {'lo': {17: [{'peer': '00:00:00:00:00:00',
-                                    'addr': '00:00:00:00:00:00'}],
-                              2: [{'peer': '127.0.0.1', 'netmask':
-                                   '255.0.0.0', 'addr': '127.0.0.1'}],
-                              10: [{'netmask': ffff,
-                                    'addr': '::1'}]
-                              },
-                       'eth0': {17: [{'broadcast': 'ff:ff:ff:ff:ff:ff',
-                                      'addr': '28:92:4a:19:8c:e8'}]
-                                },
-                       'eth2': {17: [{'broadcast': 'ff:ff:ff:ff:ff:ff',
-                                      'addr': 'e0:06:e6:41:dd:dd'}],
-                                2: [{'broadcast': '192.168.1.255',
-                                     'netmask': '255.255.255.0',
-                                     'addr': '192.168.1.108'}],
-                                10: [{'netmask': 'ffff:ffff:ffff:ffff::',
-                                      'addr': 'fe80::e206:e6ff:fe41:dddd%eth2'}
-                                     ]
-                                },
-                       }
-            return results[iface]
+            return DUMMY_ADDRESSES[iface]
 
         with mock.patch.object(netifaces, 'interfaces') as interfaces:
-            interfaces.return_value = ['lo', 'eth0', 'eth2']
+            interfaces.return_value = DUMMY_ADDRESSES.keys()
             with mock.patch.object(netifaces, 'ifaddresses') as ifaddresses:
                 ifaddresses.side_effect = side_effect
                 if not fatal:
@@ -86,8 +73,12 @@ class IPTest(unittest.TestCase):
         self.assertRaises(SystemExit, self._test_get_address_in_network,
                           None, None, fatal=True)
 
-    def test_get_address_in_network_works(self):
-        self._test_get_address_in_network('192.168.1.108', '192.168.1.0/24')
+    def test_get_address_in_network_ipv4(self):
+        self._test_get_address_in_network('192.168.1.55', '192.168.1.0/24')
+
+    def test_get_address_in_network_ipv6(self):
+        self._test_get_address_in_network('2a01:348:2f4:0:685e:5748:ae62:209f',
+                                          '2a01:348:2f4::/64')
 
     def test_get_address_in_network_with_non_existent_net(self):
         self._test_get_address_in_network(None, '172.16.0.0/16')
