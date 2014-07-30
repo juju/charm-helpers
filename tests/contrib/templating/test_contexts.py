fd8df793103d8181cb7dbf4ba84aca059b561b86
Simon Davy <bloodearnest@gmail.com>
2014-07-30 13:30:43 +0000

    Make ansible context have a more useful relations dict structure

--- tests/contrib/templating/test_contexts.py
+++ tests/contrib/templating/test_contexts.py
@@ -85,8 +85,12 @@ class JujuState2YamlTestCase(unittest.TestCase):
                 "charm_dir": "/tmp/charm_dir",
                 "group_code_owner": "webops_deploy",
                 "user_code_runner": "ubunet",
-                "relations_deprecated": {},
                 "current_relation": {},
+                "relations_full": {
+                    'wsgi-file': {},
+                    'website': {},
+                    'nrpe-external-master': {},
+                },
                 "relations": {
                     'wsgi-file': {},
                     'website': {},
@@ -115,8 +119,12 @@ class JujuState2YamlTestCase(unittest.TestCase):
                 "group_code_owner": "webops_deploy",
                 "user_code_runner": "ubunet",
                 "local_unit": "click-index/3",
-                "relations_deprecated": {},
                 "current_relation": {},
+                "relations_full": {
+                    'wsgi-file': {},
+                    'website': {},
+                    'nrpe-external-master': {},
+                },
                 "relations": {
                     'wsgi-file': {},
                     'website': {},
@@ -142,7 +150,7 @@ class JujuState2YamlTestCase(unittest.TestCase):
                     u'gunicorn/1': {
                         u'private-address': u'10.0.3.99',
                     },
-                    'your-wsgi-service/0': {
+                    'click-index/3': {
                         u'wsgi_group': u'ubunet',
                     },
                 },
@@ -164,19 +172,29 @@ class JujuState2YamlTestCase(unittest.TestCase):
                 "wsgi_file:relation_key1": "relation_value1",
                 "wsgi_file:relation_key2": "relation_value2",
                 "local_unit": "click-index/3",
-                "relations_deprecated": {"wsgi_file": []},
                 "current_relation": {
                     "relation_key1": "relation_value1",
                     "relation_key2": "relation_value2",
+                },
+                "relations_full": {
+                    'wsgi-file': {
+                        u'wsgi-file:0': {
+                            u'gunicorn/1': {
+                                u'private-address': u'10.0.3.99',
+                            },
+                            'click-index/3': {
+                                u'wsgi_group': u'ubunet',
+                            },
+                        },
+                    },
+                    'website': {},
+                    'nrpe-external-master': {},
                 },
                 "relations": {
                     'wsgi-file': {
                         u'wsgi-file:0': {
                             u'gunicorn/1': {
                                 u'private-address': u'10.0.3.99',
-                            },
-                            'your-wsgi-service/0': {
-                                u'wsgi_group': u'ubunet',
                             },
                         },
                     },
@@ -186,43 +204,6 @@ class JujuState2YamlTestCase(unittest.TestCase):
                 "unit_private_address": "10.0.3.2",
                 "unit_public_address": "123.123.123.123",
             }, result)
-
-    def test_output_with_multiple_relations(self):
-        self.mock_config.return_value = {
-            'group_code_owner': 'webops_deploy',
-            'user_code_runner': 'ubunet',
-        }
-        self.mock_relation_type.return_value = 'cluster'
-        self.mock_relation_get.return_value = {
-            'private-address': '10.0.3.105',
-        }
-        self.mock_local_unit.return_value = "click-index/3"
-        self.mock_relations_of_type.return_value = [{
-            u'private-address': u'10.0.3.105',
-            '__unit__': u'elasticsearch/1',
-            '__relid__': u'cluster:0',
-        }, {
-            u'private-address': u'10.0.3.107',
-            '__unit__': u'elasticsearch/2',
-            '__relid__': u'cluster:0',
-        }]
-
-        charmhelpers.contrib.templating.contexts.juju_state_to_yaml(
-            self.context_path)
-
-        with open(self.context_path, 'r') as context_file:
-            result = yaml.load(context_file.read())
-            self.assertIn('relations_deprecated', result)
-            self.assertIn('cluster', result['relations_deprecated'])
-            self.assertEqual([{
-                u'private_address': u'10.0.3.105',
-                '__unit__': u'elasticsearch/1',
-                '__relid__': u'cluster:0',
-            }, {
-                u'private_address': u'10.0.3.107',
-                '__unit__': u'elasticsearch/2',
-                '__relid__': u'cluster:0',
-            }], result['relations_deprecated']['cluster'])
 
     def test_relation_with_separator(self):
         self.mock_config.return_value = {
@@ -248,11 +229,15 @@ class JujuState2YamlTestCase(unittest.TestCase):
                 "wsgi_file__relation_key1": "relation_value1",
                 "wsgi_file__relation_key2": "relation_value2",
                 "local_unit": "click-index/3",
-                "relations_deprecated": {"wsgi_file": []},
                 "current_relation": {
                     "relation_key1": "relation_value1",
                     "relation_key2": "relation_value2",
                 },
+                "relations_full": {
+                    'wsgi-file': {},
+                    'website': {},
+                    'nrpe-external-master': {},
+                },
                 "relations": {
                     'wsgi-file': {},
                     'website': {},
@@ -260,68 +245,6 @@ class JujuState2YamlTestCase(unittest.TestCase):
                 },
                 "unit_private_address": "10.0.3.2",
                 "unit_public_address": "123.123.123.123",
-            }, result)
-
-    def test_updates_existing_values(self):
-        """Data stored in the context file is retained.
-
-        This may be helpful so that templates can access information
-        from relations outside the current execution environment.
-        XXX Remove. As all relation data is avaliable in any hook,
-        we're now creating the relations entry whenever a hook is
-        run (to avoid having to do any book-keeping for relations
-        departing etc.) For the moment, we'll leave the functionality
-        on the relations_deprecated property.
-        """
-        os.makedirs(os.path.dirname(self.context_path))
-        with open(self.context_path, 'w+') as context_file:
-            context_file.write(yaml.dump({
-                'solr:hostname': 'example.com',
-                'user_code_runner': 'oldvalue',
-                'relations_deprecated': {
-                    'website': [{u'private_address': u'10.0.3.107'}],
-                }
-            }))
-
-        self.mock_config.return_value = charmhelpers.core.hookenv.Serializable({
-            'group_code_owner': 'webops_deploy',
-            'user_code_runner': 'newvalue',
-        })
-        self.mock_local_unit.return_value = "click-index/3"
-        self.mock_relation_type.return_value = 'cluster'
-        self.mock_relations_of_type.return_value = [{
-            u'private-address': u'10.0.3.105',
-        }]
-        self.mock_relation_get.return_value = {
-            u'private-address': u'10.0.3.105',
-        }
-
-        charmhelpers.contrib.templating.contexts.juju_state_to_yaml(
-            self.context_path)
-
-        with open(self.context_path, 'r') as context_file:
-            result = yaml.load(context_file.read())
-            self.assertEqual({
-                "charm_dir": "/tmp/charm_dir",
-                "group_code_owner": "webops_deploy",
-                "user_code_runner": "newvalue",
-                "local_unit": "click-index/3",
-                "solr:hostname": "example.com",
-                "cluster:private_address": "10.0.3.105",
-                "relations_deprecated": {
-                    'website': [{u'private_address': u'10.0.3.107'}],
-                    'cluster': [{u'private_address': u'10.0.3.105'}],
-                },
-                "unit_private_address": "10.0.3.2",
-                "unit_public_address": "123.123.123.123",
-                "current_relation": {
-                    'private-address': '10.0.3.105',
-                },
-                "relations": {
-                    'wsgi-file': {},
-                    'website': {},
-                    'nrpe-external-master': {},
-                }
             }, result)
 
     def test_keys_with_hyphens(self):
@@ -342,8 +265,12 @@ class JujuState2YamlTestCase(unittest.TestCase):
                 "charm_dir": "/tmp/charm_dir",
                 "group_code_owner": "webops_deploy",
                 "user_code_runner": "ubunet",
-                "relations_deprecated": {},
                 'current_relation': {},
+                "relations_full": {
+                    'wsgi-file': {},
+                    'website': {},
+                    'nrpe-external-master': {},
+                },
                 "relations": {
                     'wsgi-file': {},
                     'website': {},
@@ -379,12 +306,16 @@ class JujuState2YamlTestCase(unittest.TestCase):
                 "group_code_owner": "webops_deploy",
                 "user_code_runner": "ubunet",
                 "local_unit": "click-index/3",
-                "relations_deprecated": {"wsgi_file": []},
                 'current_relation': {
                     'relation-key1': 'relation_value1',
                     'relation-key2': 'relation_value2',
                 },
                 "relations": {
+                    'wsgi-file': {},
+                    'website': {},
+                    'nrpe-external-master': {},
+                },
+                "relations_full": {
                     'wsgi-file': {},
                     'website': {},
                     'nrpe-external-master': {},
