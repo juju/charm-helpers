68b821600b160e3b9b7615d0d440774c3ece3e89
Liam Young <liam.young@canonical.com>
2014-08-19 09:38:07 +0000

    Added peer_retrieve_by_prefix function to return all k/v pairs from the peer db given a prefix and filter using {inc,exc}_list.

--- charmhelpers/contrib/peerstorage/__init__.py
+++ charmhelpers/contrib/peerstorage/__init__.py
@@ -51,6 +51,22 @@ def peer_retrieve(key, relation_name='cluster'):
                          'peer relation {}'.format(relation_name))
 
 
+def peer_retrieve_by_prefix(prefix, relation_name='cluster', delimiter='_',
+                            inc_list=[], exc_list=[]):
+    """ Retrieve k/v pairs given a prefix and filter using {inc,exc}_list """
+    peerdb_settings = peer_retrieve('-', relation_name=relation_name)
+    matched = {}
+    for k, v in peerdb_settings.items():
+        full_prefix = prefix + delimiter
+        if k.startswith(full_prefix):
+            new_key = k.replace(full_prefix, '')
+            if new_key in exc_list:
+                continue
+            if new_key in inc_list or len(inc_list) == 0:
+                matched[new_key] = v
+    return matched
+
+
 def peer_store(key, value, relation_name='cluster'):
     """ Store the key/value pair on the named peer relation relation_name """
     cluster_rels = relation_ids(relation_name)
@@ -86,7 +102,8 @@ def peer_echo(includes=None):
 
 
 def peer_store_and_set(relation_id=None, peer_relation_name='cluster',
-                       peer_store_fatal=False, relation_settings={}, **kwargs):
+                       peer_store_fatal=False, relation_settings={},
+                       delimiter='_', **kwargs):
     """ For each pair set them in the relation and store in peer db
 
     Note that the relation set is done within the provided relation_id and
@@ -98,7 +115,7 @@ def peer_store_and_set(relation_id=None, peer_relation_name='cluster',
         for key, value in dict(kwargs.items() +
                                relation_settings.items()).iteritems():
             key_prefix = relation_id or current_relation_id()
-            peer_store(key_prefix + '_' + key,
+            peer_store(key_prefix + delimiter + key,
                        value,
                        relation_name=peer_relation_name)
     else:
