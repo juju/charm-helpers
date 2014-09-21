ececb23b1cd16a24c574be56bfffbec54dbfec2e
Hui Xiang <hui.xiang@canonical.com>
2014-09-21 20:54:17 +0800

    Excludes vip.

--- charmhelpers/contrib/openstack/ip.py
+++ charmhelpers/contrib/openstack/ip.py
@@ -66,7 +66,10 @@ def resolve_address(endpoint_type=PUBLIC):
                     resolved_address = vip
     else:
         if config('prefer-ipv6'):
-            fallback_addr = get_ipv6_addr()
+            list = []
+            if config('vip'):
+                list.append(config('vip'))
+            fallback_addr = get_ipv6_addr(exc_list=list)[0]
         else:
             fallback_addr = unit_get(_address_map[endpoint_type]['fallback'])
         resolved_address = get_address_in_network(
