dae5706440d3fb63710ed3b50218a6260baef4be
Edward Hope-Morley <edward.hope-morley@canonical.com>
2014-09-26 19:35:37 +0100

    sync_db_with_multi_ipv6_addresses() now takes db and user as args

--- tests/contrib/openstack/test_os_utils.py
+++ tests/contrib/openstack/test_os_utils.py
@@ -10,55 +10,36 @@ class UtilsTests(unittest.TestCase):
         super(UtilsTests, self).setUp()
 
     @mock.patch('charmhelpers.contrib.openstack.utils.relation_set')
-    @mock.patch('charmhelpers.contrib.openstack.utils.config')
     @mock.patch('charmhelpers.contrib.openstack.utils.relation_ids')
     @mock.patch('charmhelpers.contrib.openstack.utils.get_ipv6_addr')
     def test_sync_db_with_multi_ipv6_addresses(self, mock_get_ipv6_addr,
-                                               mock_relation_ids, mock_config,
+                                               mock_relation_ids,
                                                mock_relation_set):
         addr1 = '2001:db8:1:0:f816:3eff:fe45:7c/64'
         addr2 = '2001:db8:1:0:d0cf:528c:23eb:5000/64'
         mock_get_ipv6_addr.return_value = [addr1, addr2]
         mock_relation_ids.return_value = ['shared-db']
 
-        config = {'database': 'big', 'database-user': 'wind'}
-
-        class cls_mock_config(object):
-
-            def __call__(self, key):
-                return config[key]
-
-        mock_config.side_effect = cls_mock_config()
-        utils.sync_db_with_multi_ipv6_addresses()
+        utils.sync_db_with_multi_ipv6_addresses('testdb', 'testdbuser')
         hosts = json.dumps([addr1, addr2])
         mock_relation_set.assert_called_with(relation_id='shared-db',
-                                             database='big',
-                                             username='wind',
+                                             database='testdb',
+                                             username='testdbuser',
                                              hostname=hosts)
 
     @mock.patch('charmhelpers.contrib.openstack.utils.relation_set')
-    @mock.patch('charmhelpers.contrib.openstack.utils.config')
     @mock.patch('charmhelpers.contrib.openstack.utils.relation_ids')
     @mock.patch('charmhelpers.contrib.openstack.utils.get_ipv6_addr')
     def test_sync_db_with_multi_ipv6_addresses_single(self, mock_get_ipv6_addr,
                                                       mock_relation_ids,
-                                                      mock_config,
                                                       mock_relation_set):
         addr1 = '2001:db8:1:0:f816:3eff:fe45:7c/64'
         mock_get_ipv6_addr.return_value = [addr1]
         mock_relation_ids.return_value = ['shared-db']
 
-        config = {'database': 'big', 'database-user': 'wind'}
-
-        class cls_mock_config(object):
-
-            def __call__(self, key):
-                return config[key]
-
-        mock_config.side_effect = cls_mock_config()
-        utils.sync_db_with_multi_ipv6_addresses()
+        utils.sync_db_with_multi_ipv6_addresses('testdb', 'testdbuser')
         hosts = json.dumps([addr1])
         mock_relation_set.assert_called_with(relation_id='shared-db',
-                                             database='big',
-                                             username='wind',
+                                             database='testdb',
+                                             username='testdbuser',
                                              hostname=hosts)
