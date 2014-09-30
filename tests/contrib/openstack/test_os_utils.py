fae7454dbd4160bae70dc9138ae27c843116bb67
Edward Hope-Morley <edward.hope-morley@canonical.com>
2014-09-30 11:16:32 +0100

    add support for relation_prefix to sync_db_with_multi_ipv6_addresses()

--- tests/contrib/openstack/test_os_utils.py
+++ tests/contrib/openstack/test_os_utils.py
@@ -43,3 +43,22 @@ class UtilsTests(unittest.TestCase):
                                              database='testdb',
                                              username='testdbuser',
                                              hostname=hosts)
+
+    @mock.patch('charmhelpers.contrib.openstack.utils.relation_set')
+    @mock.patch('charmhelpers.contrib.openstack.utils.relation_ids')
+    @mock.patch('charmhelpers.contrib.openstack.utils.get_ipv6_addr')
+    def test_sync_db_with_multi_ipv6_addresses_w_prefix(self,
+                                                        mock_get_ipv6_addr,
+                                                        mock_relation_ids,
+                                                        mock_relation_set):
+        addr1 = '2001:db8:1:0:f816:3eff:fe45:7c/64'
+        mock_get_ipv6_addr.return_value = [addr1]
+        mock_relation_ids.return_value = ['shared-db']
+
+        utils.sync_db_with_multi_ipv6_addresses('testdb', 'testdbuser',
+                                                relation_prefix='bungabunga')
+        hosts = json.dumps([addr1])
+        mock_relation_set.assert_called_with(relation_id='shared-db',
+                                             bungabunga_database='testdb',
+                                             bungabunga_username='testdbuser',
+                                             bungabunga_hostname=hosts)
