4a0d5a41b4489daee4528ac529315001e2fe15d2
Stuart Bishop <stuart@stuartbishop.net>
2014-12-16 21:37:45 +0700

    Tidy a test

--- tests/core/test_templating.py
+++ tests/core/test_templating.py
@@ -1,4 +1,3 @@
-import os
 import pkg_resources
 import tempfile
 import unittest
@@ -14,7 +13,8 @@ TEMPLATES_DIR = pkg_resources.resource_filename(__name__, 'templates')
 class TestTemplating(unittest.TestCase):
     def setUp(self):
         self.charm_dir = pkg_resources.resource_filename(__name__, '')
-        self._charm_dir_patch = mock.patch.object(templating.hookenv, 'charm_dir')
+        self._charm_dir_patch = mock.patch.object(templating.hookenv,
+                                                  'charm_dir')
         self._charm_dir_mock = self._charm_dir_patch.start()
         self._charm_dir_mock.side_effect = lambda: self.charm_dir
 
@@ -25,9 +25,8 @@ class TestTemplating(unittest.TestCase):
     @mock.patch.object(templating.host, 'mkdir')
     @mock.patch.object(templating.host, 'log')
     def test_render(self, log, mkdir, fchown):
-        _, fn1 = tempfile.mkstemp()
-        _, fn2 = tempfile.mkstemp()
-        try:
+        with tempfile.NamedTemporaryFile() as fn1, \
+                tempfile.NamedTemporaryFile() as fn2:
             context = {
                 'nats': {
                     'port': '1234',
@@ -38,29 +37,27 @@ class TestTemplating(unittest.TestCase):
                 },
                 'nginx_port': 80,
             }
-            templating.render('fake_cc.yml', fn1,
+            templating.render('fake_cc.yml', fn1.name,
                               context, templates_dir=TEMPLATES_DIR)
-            contents = open(fn1).read()
+            contents = open(fn1.name).read()
             self.assertRegexpMatches(contents, 'port: 1234')
             self.assertRegexpMatches(contents, 'host: example.com')
             self.assertRegexpMatches(contents, 'domain: api.foo.com')
 
-            templating.render('test.conf', fn2, context,
+            templating.render('test.conf', fn2.name, context,
                               templates_dir=TEMPLATES_DIR)
-            contents = open(fn2).read()
+            contents = open(fn2.name).read()
             self.assertRegexpMatches(contents, 'listen 80')
             self.assertEqual(fchown.call_count, 2)
             self.assertEqual(mkdir.call_count, 2)
-        finally:
-            for fn in (fn1, fn2):
-                if os.path.exists(fn):
-                    os.remove(fn)
 
     @mock.patch.object(templating, 'hookenv')
     @mock.patch('jinja2.Environment')
     def test_load_error(self, Env, hookenv):
-        Env().get_template.side_effect = jinja2.exceptions.TemplateNotFound('fake_cc.yml')
+        Env().get_template.side_effect = jinja2.exceptions.TemplateNotFound(
+            'fake_cc.yml')
         self.assertRaises(
             jinja2.exceptions.TemplateNotFound, templating.render,
             'fake.src', 'fake.tgt', {}, templates_dir='tmpl')
-        hookenv.log.assert_called_once_with('Could not load template fake.src from tmpl.', level=hookenv.ERROR)
+        hookenv.log.assert_called_once_with(
+            'Could not load template fake.src from tmpl.', level=hookenv.ERROR)
