55c404a502fac67c730c69398dc657aa0045f47f
Kapil Thangavelu <kapil@canonical.com>
2015-01-21 11:54:02 -0500

    address review comments, add more documentation

--- charmhelpers/core/unitdata.py
+++ charmhelpers/core/unitdata.py
@@ -8,20 +8,77 @@
 #  Kapil Thangavelu <kapil.foss@gmail.com>
 #
 """
+Intro
+-----
+
 A simple way to carry state in units, supports versioned
 and transactional operation.
 
-from unitdata import kv
-with kv.hook_scope():
-   # do work
-   kv.set('x', 1)
 
-will automatically operate in a transaction.
+Usage
+-----
+The preferred usage mode is via context manager::
+
+  >>> from unitdata import kv
+  >>> db = kv()
+  >>> with db.hook_scope('install'):
+  ...    # do work
+  ...    db.set('x', 1)
+  >>> db.get('x')
+  1
+
+will automatically use a transaction for operations within the with
+block scope. It will also record the current hook name and timestamp
+to provide additional context when looking at historical values.
+
+Values are automatically json de/serialized to preserve basic typing
+capabilities (ints, booleans, etc).
+
+Individual values can be manipulated via get/set::
+
+   >>> kv.set('y', True)
+   >>> kv.get('y')
+   True
+
+Groups of keys can be manipulated with update/getrange::
+
+   >>> kv.update({'z': 1, 'y': 2}, prefix="gui.")
+   >>> kv.getrange('gui.', strip=True)
+   {'z': 1, 'y': 2}
+
+When updating values, its very helpful to understand which values
+have actually changed and how have they changed. The storage
+provides a delta method to provide for this::
+
+   >>> data = {'debug': True, 'option': 2}
+   >>> delta = kv.delta(data, 'config.')
+   >>> delta.debug.previous
+   None
+   >>> delta.debug.current
+   True
+   >>> delta
+   {'debug': (None, True), 'option': (None, 2)}
+
+Note the delta method does not persist the actual change, it needs to
+be explicitly saved via 'update' method::
+
+   >>> kv.update(data, 'config.')
+
+Values modified in the context of a hook scope retain historical values
+associated to the hookname. History does not automatically json decode
+values.
+
+   >>> with db.hook_scope('config-changed'):
+   ...      db.set('x', 42)
+   >>> db.gethistory('x')
+   [(1, u'x', u'1', u'install', u'2015-01-21T16:49:30.038372'),
+    (2, u'x', u'42', u'config-changed', u'2015-01-21T16:49:30.038786')]
+
 """
 
 __author__ = 'Kapil Thangavelu <kapil.foss@gmail.com>'
 
-import atexit
+import collections
 import contextlib
 import datetime
 import json
@@ -48,15 +105,19 @@ class Storage(object):
         self.conn = sqlite3.connect('%s' % self.db_path)
         self.cursor = self.conn.cursor()
         self.revision = None
+        self._closed = False
         self._init()
+
+    def close(self):
+        if self._closed:
+            return
+        self.flush(False)
+        self.cursor.close()
+        self.conn.close()
 
     def _scoped_query(self, stmt, params=None):
         if params is None:
             params = []
-        return stmt, params
-        if self.revision:
-            stmt += " and revision=?"
-            params.append(self.revision)
         return stmt, params
 
     def get(self, key, default=None, record=False):
@@ -71,7 +132,7 @@ class Storage(object):
         return json.loads(result[0])
 
     def getrange(self, key_prefix, strip=False):
-        stmt = "select key, data from kv where key like '%s_%%'" % key_prefix
+        stmt = "select key, data from kv where key like '%s%%'" % key_prefix
         self.cursor.execute(*self._scoped_query(stmt))
         result = self.cursor.fetchall()
 
@@ -142,43 +203,44 @@ class Storage(object):
 
     def delta(self, mapping, prefix):
         """
-        Store mapping and return a delta containing values that
-        have changed.
-
+        return a delta containing values that have changed.
         """
         previous = self.getrange(prefix, strip=True)
-        pk = set(previous.keys())
+        if not previous:
+            pk = set()
+        else:
+            pk = set(previous.keys())
         ck = set(mapping.keys())
         delta = DeltaSet()
 
         # added
         for k in ck.difference(pk):
-            delta[k] = Delta((None, mapping[k]))
+            delta[k] = Delta(None, mapping[k])
 
         # removed
         for k in pk.difference(ck):
-            delta[k] = Delta((previous[k], None))
+            delta[k] = Delta(previous[k], None)
 
         # changed
         for k in pk.intersection(ck):
             c = mapping[k]
             p = previous[k]
             if c != p:
-                delta[k] = Delta((p, c))
+                delta[k] = Delta(p, c)
 
         if delta:
             return delta
         return None
 
     @contextlib.contextmanager
-    def hook_scope(self, name, context=True):
+    def hook_scope(self, name="", context=True):
         """Scope all future interactions to the current hook execution
         revision."""
         assert not self.revision
         self.cursor.execute(
             'insert into hooks (hook, date) values (?, ?)',
-            (sys.argv[0],
-             datetime.datetime.now().isoformat()))
+            (name or sys.argv[0],
+             datetime.datetime.utcnow().isoformat()))
         self.revision = self.cursor.lastrowid
         if not context:
             yield self.revision
@@ -196,43 +258,48 @@ class Storage(object):
     def flush(self, save=True):
         if save:
             self.conn.commit()
+        elif self._closed:
+            return
         else:
             self.conn.rollback()
 
     def _init(self):
         self.cursor.execute('''
-            create table kv (
+            create table if not exists kv (
                key text,
                data text,
                primary key (key)
                )''')
         self.cursor.execute('''
-            create table kv_revisions (
+            create table if not exists kv_revisions (
                key text,
                revision integer,
                data text,
                primary key (key, revision)
                )''')
         self.cursor.execute('''
-            create table hooks (
+            create table if not exists hooks (
                version integer primary key autoincrement,
                hook text,
                date text
                )''')
         self.conn.commit()
-        atexit.register(self.conn.commit)
 
     def gethistory(self, key):
         self.cursor.execute(
-            'select revision, key, data from kv_revisions where key=?', [key])
+            '''
+            select kv.revision, kv.key, kv.data, h.hook, h.date
+            from kv_revisions kv,
+                 hooks h
+            where kv.key=?
+             and kv.revision = h.version
+            ''', [key])
         return self.cursor.fetchall()
 
     def debug(self):
         self.cursor.execute('select * from kv')
         pprint.pprint(self.cursor.fetchall())
-
         self.cursor.execute('select * from kv_revisions')
-        import pprint
         pprint.pprint(self.cursor.fetchall())
 
 
@@ -246,28 +313,19 @@ class Record(dict):
         raise AttributeError(k)
 
 
-class DeltaSet(dict):
+class DeltaSet(Record):
 
     __slots__ = ()
 
-    def __getattr__(self, k):
-        if k in self:
-            return self[k]
-        raise AttributeError(k)
+
+Delta = collections.namedtuple('Delta', ['previous', 'current'])
 
 
-class Delta(tuple):
-
-    __slots__ = ()
-
-    @property
-    def current(self):
-        return self[1]
-
-    @property
-    def previous(self):
-        return self[0]
+_KV = None
 
 
-if 'CHARM_DIR' in os.environ:
-    kv = Storage()
+def kv():
+    global _KV
+    if _KV is None:
+        _KV = Storage()
+    return _KV
