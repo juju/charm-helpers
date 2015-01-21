6153d4dae3546770c33d305de75a865899cceb79
Kapil Thangavelu <kapil@canonical.com>
2015-01-21 11:54:02 -0500

    address review comments, add more documentation

--- tests/core/test_unitdata.py
+++ tests/core/test_unitdata.py
@@ -6,39 +6,76 @@
 #  Kapil Thangavelu <kapil.foss@gmail.com>
 #
 import unittest
+import tempfile
 
 from charmhelpers.core.unitdata import Storage
 
 
 class StorageTest(unittest.TestCase):
 
+    def test_init_kv_multiple(self):
+        with tempfile.NamedTemporaryFile() as fh:
+            kv = Storage(fh.name)
+            with kv.hook_scope('xyz'):
+                kv.set('x', 1)
+            kv.close()
+
+            kv = Storage(fh.name)
+            with kv.hook_scope('abc'):
+                self.assertEqual(kv.get('x'), 1)
+            kv.close()
+
     def test_hook_scope(self):
         kv = Storage(':memory:')
         try:
-            with kv.hook_scope('xyz') as rev:
+            with kv.hook_scope('install') as rev:
                 self.assertEqual(rev, 1)
                 kv.set('a', 1)
                 raise RuntimeError('x')
         except RuntimeError:
             self.assertEqual(kv.get('a'), None)
 
-        with kv.hook_scope('xyz') as rev:
+        with kv.hook_scope('config-changed') as rev:
             self.assertEqual(rev, 1)
             kv.set('a', 1)
         self.assertEqual(kv.get('a'), 1)
 
         kv.revision = None
 
-        with kv.hook_scope('xyz') as rev:
+        with kv.hook_scope('start') as rev:
             self.assertEqual(rev, 2)
             kv.set('a', False)
             kv.set('a', True)
         self.assertEqual(kv.get('a'), True)
 
         # History doesn't decode atm
+        history = [h[:-1] for h in kv.gethistory('a')]
         self.assertEqual(
-            kv.gethistory('a'),
-            [(1, 'a', '1'), (2, 'a', 'true')])
+            history,
+            [(1, 'a', '1', 'config-changed'),
+             (2, 'a', 'true', 'start')])
+
+    def test_delta_no_previous_and_history(self):
+        kv = Storage(':memory:')
+        with kv.hook_scope('install'):
+            data = {'a': 0, 'c': False}
+            delta = kv.delta(data, 'settings.')
+            self.assertEqual(delta, {
+                'a': (None, False), 'c': (None, False)})
+            kv.update(data, 'settings.')
+
+        with kv.hook_scope('config'):
+            data = {'a': 1, 'c': True}
+            delta = kv.delta(data, 'settings.')
+            self.assertEqual(delta, {
+                'a': (0, 1), 'c': (False, True)})
+            kv.update(data, 'settings.')
+        # strip the time
+        history = [h[:-1] for h in kv.gethistory('settings.a')]
+        self.assertEqual(
+            history,
+            [(1, 'settings.a', '0', 'install'),
+             (2, 'settings.a', '1', 'config')])
 
     def test_delta(self):
         kv = Storage(':memory:')
