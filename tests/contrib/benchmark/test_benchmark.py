a4b0b15c7d7bc07b21753e5845ca33f530282cca
Adam Israel <adam.israel@gmail.com>
2015-03-16 21:26:13 -0400

    100% coverage. Implements patch_open and FakeRelation from tests.helper.

--- tests/contrib/benchmark/test_benchmark.py
+++ tests/contrib/benchmark/test_benchmark.py
@@ -1,44 +1,46 @@
 import mock
-# import os
-# import subprocess
-import unittest
-# from charmhelpers.core import hookenv
+from testtools import TestCase
+# import unittest
 from charmhelpers.contrib.benchmark import Benchmark, action_set  # noqa
-# from tests.helpers import patch_open, FakeRelation
+from tests.helpers import patch_open, FakeRelation
 
-# FAKE_RELATION = {
-#     'benchmark:0': {
-#         'benchmark/0': {
-#             'hostname': '127.0.0.1',
-#             'port': '1111',
-#             'graphite_port': '2222',
-#             'graphite_endpoint': 'http://localhost:3333',
-#             'api_port': '4444'
-#         }
-#     }
-# }
-#
-# TO_PATCH = [
-#     'in_relation_hook',
-#     'relation_ids',
-#     'relation_set',
-#     'relation_get',
-# ]
+TO_PATCH = [
+    'in_relation_hook',
+    'relation_ids',
+    'relation_set',
+    'relation_get',
+]
+
+FAKE_RELATION = {
+    'benchmark:0': {
+        'benchmark/0': {
+            'hostname': '127.0.0.1',
+            'port': '1111',
+            'graphite_port': '2222',
+            'graphite_endpoint': 'http://localhost:3333',
+            'api_port': '4444'
+        }
+    }
+}
 
 
-class TestBenchmark(unittest.TestCase):
+class TestBenchmark(TestCase):
 
-    # def setUp(self):
-    #     super(TestBenchmark, self).setUp()
-    #     for m in TO_PATCH:
-    #         setattr(self, m, self._patch(m))
-    #
-    # def _patch(self, method):
-    #     _m = mock.patch('charmhelpers.contrib.benchmark.' + method)
-    #     m = _m.start()
-    #     self.addCleanup(_m.stop)
-    #     return m
-    #
+    def setUp(self):
+        super(TestBenchmark, self).setUp()
+        for m in TO_PATCH:
+            setattr(self, m, self._patch(m))
+        self.fake_relation = FakeRelation(FAKE_RELATION)
+        # self.hook_name.return_value = 'benchmark-relation-changed'
+
+        self.relation_get.side_effect = self.fake_relation.get
+        self.relation_ids.side_effect = self.fake_relation.relation_ids
+
+    def _patch(self, method):
+        _m = mock.patch('charmhelpers.contrib.benchmark.' + method)
+        m = _m.start()
+        self.addCleanup(_m.stop)
+        return m
 
     @mock.patch('os.path.exists')
     @mock.patch('subprocess.check_output')
@@ -47,14 +49,18 @@ class TestBenchmark(unittest.TestCase):
         exists.return_value = True
         check_output.return_value = "data"
 
-        self.assertIsNone(Benchmark().start())
+        with patch_open() as (_open, _file):
+            self.assertIsNone(Benchmark().start())
+            # _open.assert_called_with('/etc/benchmark.conf', 'w')
 
         COLLECT_PROFILE_DATA = '/usr/local/bin/collect-profile-data'
         exists.assert_any_call(COLLECT_PROFILE_DATA)
         check_output.assert_any_call([COLLECT_PROFILE_DATA])
 
     def test_benchmark_finish(self):
-        self.assertIsNone(Benchmark().finish())
+        with patch_open() as (_open, _file):
+            self.assertIsNone(Benchmark().finish())
+            # _open.assert_called_with('/etc/benchmark.conf', 'w')
 
     @mock.patch('charmhelpers.contrib.benchmark.find_executable')
     @mock.patch('charmhelpers.contrib.benchmark.subprocess.check_call')
@@ -76,43 +82,34 @@ class TestBenchmark(unittest.TestCase):
         check_call.assert_any_call(['action-set', 'baz.foo=1'])
         check_call.assert_any_call(['action-set', 'baz.bar=2'])
 
-    @mock.patch('__builtin__.open')
     @mock.patch('charmhelpers.contrib.benchmark.relation_get')
     @mock.patch('charmhelpers.contrib.benchmark.relation_set')
     @mock.patch('charmhelpers.contrib.benchmark.relation_ids')
     @mock.patch('charmhelpers.contrib.benchmark.in_relation_hook')
-    def test_benchmark_init(self, in_relation_hook, relation_ids, relation_set, relation_get, fopen):
+    def test_benchmark_init(self, in_relation_hook, relation_ids, relation_set, relation_get):
 
         in_relation_hook.return_value = True
-        relation_ids.return_value = ['benchmark:1']
-        relation_get.return_value = None
+        relation_ids.return_value = ['benchmark:0']
         actions = ['asdf', 'foobar']
 
-        # rel = FakeRelation(IDENTITY_NEW_STYLE_CERTS)
-        # self.relation_ids.side_effect = rel.relation_ids
-        # self.relation_list.side_effect = rel.relation_units
-        # self.relation_get.side_effect = rel.get
-        # result = apache_utils.get_cert('test-cn')
+        with patch_open() as (_open, _file):
+            b = Benchmark(actions)
 
-        # r = FakeRelation(relation_data=rel)
-        # self.relation_get.side_effect = r.get
-        # rel_benchmark = self.relation_get('benchmark')
+            self.assertIsInstance(b, Benchmark)
 
-        b = Benchmark(actions)
+            self.assertTrue(relation_get.called)
+            self.assertTrue(relation_set.called)
 
-        # for key in b.required_keys:
-        #     relation_get.assert_called_once_with
+            relation_ids.assert_called_once_with('benchmark')
 
-        self.assertIsInstance(b, Benchmark)
-        relation_set.assert_called_once_with(
-            relation_id='benchmark:1',
-            relation_settings={'benchmarks': ",".join(actions)}
-        )
+            for key in b.required_keys:
+                relation_get.assert_any_call(key)
 
-        relation_ids.assert_called_once_with('benchmark')
+            relation_set.assert_called_once_with(
+                relation_id='benchmark:0',
+                relation_settings={'benchmarks': ",".join(actions)}
+            )
 
-        # m.assert_called_once_with('/etc/benchmark.conf', 'w')
-        # m().write.assert_called_once_with('stuff')
-
-        # with open('/etc/benchmark.conf', 'w') as f:
-        #     f.write('stuff')
+            _open.assert_called_with('/etc/benchmark.conf', 'w')
+            for key, val in FAKE_RELATION['benchmark:0']['benchmark/0'].iteritems():
+                _file.write.assert_any_called("%s=%s\n" % (key, val))
