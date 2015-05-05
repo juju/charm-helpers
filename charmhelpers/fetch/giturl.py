e86ef4af3bb7557764d418798ee7db7d4f49d292
Corey Bryant <corey.bryant@canonical.com>
2015-05-05 16:58:53 +0000

    Fix lint error

--- charmhelpers/fetch/giturl.py
+++ charmhelpers/fetch/giturl.py
@@ -50,9 +50,9 @@ class GitUrlFetchHandler(BaseFetchHandler):
             raise UnhandledSource("Cannot handle {}".format(source))
 
         if depth:
-            repo = Repo.clone_from(source, dest, branch=branch, depth=depth)
+            Repo.clone_from(source, dest, branch=branch, depth=depth)
         else:
-            repo = Repo.clone_from(source, dest, branch=branch)
+            Repo.clone_from(source, dest, branch=branch)
 
     def install(self, source, branch="master", dest=None, depth=None):
         url_parts = self.parse_url(source)
