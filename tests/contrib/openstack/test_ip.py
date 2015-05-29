b7405e6196017a093b692d5a46b8a26ec1ea543a
billy.olsen@canonical.com <>
2015-05-29 13:23:05 -0700

    After studying the code across the OpenStack charms, it seems a better place for the override is to place it in the canonical_url method. This has the advantage of capturing more places besides endpoint URLs (e.g. also for console hosts etc) and requires fewer changes to most of the OpenStack charms.

--- tests/contrib/openstack/test_ip.py
+++ tests/contrib/openstack/test_ip.py
@@ -110,41 +110,10 @@ class IPTestCase(TestCase):
         self.assertTrue(ip.canonical_url(configs),
                         'https://unit1')
 
-    def test_endpoint_url(self):
+    def test_canonical_url_override(self):
         configs = MagicMock()
         configs.complete_contexts.return_value = ['https']
-
-        self.test_config.set('public-address', 'public.example.com')
-        self.test_config.set('internal-address', 'internal.example.com')
-        self.test_config.set('admin-address', 'admin.example.com')
-
-        endpoint = ip.public_endpoint(configs, '%s:%s/pub', 443)
-        self.assertEqual('https://public.example.com:443/pub',
+        self.test_config.set('endpoint-public-name', 'public.example.com')
+        endpoint = ip.canonical_url(configs)
+        self.assertEqual('https://public.example.com',
                          endpoint)
-
-        endpoint = ip.internal_endpoint(configs, '%s:%s/internal', 443)
-        self.assertEqual('https://internal.example.com:443/internal',
-                         endpoint)
-
-        endpoint = ip.admin_endpoint(configs, '%s:%s/admin', 443)
-        self.assertEqual('https://admin.example.com:443/admin',
-                         endpoint)
-
-    @patch.object(ip, 'canonical_url')
-    def test_endpoint_url_no_override(self, canonical_url):
-        canonical_url.return_value = 'https://unit1'
-        configs = MagicMock()
-        configs.complete_contexts.return_value = ['https']
-
-        endpoint = ip.endpoint_url(configs, '%s:%s/v1/AUTH_$(tenant_id)s', 443)
-        self.assertEqual('https://unit1:443/v1/AUTH_$(tenant_id)s', endpoint)
-
-    @patch.object(ip, 'resolve_address')
-    @patch.object(ip, 'is_ipv6')
-    def test_endpoint_url_no_override_ipv6(self, is_ipv6, resolve_address):
-        resolve_address.return_value = 'unit1'
-        is_ipv6.return_value = True
-        configs = MagicMock()
-        configs.complete_contexts.return_value = ['https']
-        endpoint = ip.endpoint_url(configs, '%s:%s', 443, ip.ADMIN)
-        self.assertEquals('https://[unit1]:443', endpoint)
