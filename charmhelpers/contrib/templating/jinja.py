17b65004d41ce7e33dbc76f0c1c455e56d3e3340
Christopher Glass <christopher.glass@canonical.com>
2015-09-18 10:24:58 +0300

    Fixed a couple of other places where this problem is likely to happen.

--- charmhelpers/contrib/templating/jinja.py
+++ charmhelpers/contrib/templating/jinja.py
@@ -18,14 +18,15 @@
 Templating using the python-jinja2 package.
 """
 import six
-from charmhelpers.fetch import apt_install
+from charmhelpers.fetch import apt_install, apt_update
 try:
     import jinja2
 except ImportError:
+    apt_update(fatal=True)
     if six.PY3:
-        apt_install(["python3-jinja2"])
+        apt_install(["python3-jinja2"], fatal=True)
     else:
-        apt_install(["python-jinja2"])
+        apt_install(["python-jinja2"], fatal=True)
     import jinja2
 
 
