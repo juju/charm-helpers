190d91af9c56186bc41ad73c6ef096cd338f1537
James Page <james.page@ubuntu.com>
2015-10-10 14:29:15 -0700

    [trivial] Add swift 2.5.0 to the range for liberty

--- charmhelpers/contrib/openstack/utils.py
+++ charmhelpers/contrib/openstack/utils.py
@@ -121,6 +121,7 @@ SWIFT_CODENAMES = OrderedDict([
     ('2.2.2', 'kilo'),
     ('2.3.0', 'liberty'),
     ('2.4.0', 'liberty'),
+    ('2.5.0', 'liberty'),
 ])
 
 # >= Liberty version->codename mapping
