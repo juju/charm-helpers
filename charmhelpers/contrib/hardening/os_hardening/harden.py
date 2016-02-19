e47d1d8e862df1600471a56eee21116a74247a71
Edward Hope-Morley <edward.hope-morley@canonical.com>
2016-02-19 15:46:39 +0000

    more

--- charmhelpers/contrib/hardening/os_hardening/harden.py
+++ charmhelpers/contrib/hardening/os_hardening/harden.py
@@ -17,6 +17,7 @@
 import os
 import platform
 import re
+import subprocess
 import yaml
 
 from charmhelpers.contrib.hardening import templating
@@ -24,6 +25,10 @@ from charmhelpers.contrib.hardening.utils import (
     ensure_permissions,
 )
 from charmhelpers.core.hookenv import config
+from charmhelpers.fetch import (
+    apt_install,
+    apt_purge,
+)
 
 OS_TEMPLATES = os.path.join(os.path.dirname(__file__), 'templates')
 
@@ -32,6 +37,41 @@ def get_defaults():
     defaults = os.path.join(os.path.dirname(__file__),
                             'defaults/main.yaml')
     return yaml.safe_load(open(defaults))
+
+
+class PAMContext(object):
+
+    def __init__(self, pam_name):
+        self.pam_name = pam_name
+
+    def __call__(self):
+        ctxt = {}
+        defaults = get_defaults()
+
+        # Always remove?
+        apt_purge('libpam-ccreds')
+
+        if self.pam_name == 'passwdqc':
+            if defaults.get('auth_pam_passwdqc_enable'):
+                apt_purge('libpam-cracklibtapt')
+                apt_install('libpam-passwdqc')
+                ctxt['auth_pam_passwdqc_options'] = \
+                    defaults.get('auth_pam_passwdqc_options')
+            else:
+                apt_purge('libpam-passwdqc')
+        elif self.pam_name == 'tally2':
+            ctxt['auth_lockout_time'] = defaults.get('auth_lockout_time')
+            if defaults.get('auth_retries'):
+                ctxt['auth_retries'] = defaults.get('auth_retries')
+                apt_install('libpam-modules')
+            else:
+                os.remove('/usr/share/pam-configs/tally2')
+                # Stop template frombeing written since we want to disable tally2
+                ctxt['__disable__'] = True
+        else:
+            raise Exception("Unrecognised PAM name '%s'" % (self.pam_name))
+
+        return ctxt
 
 
 class ModulesContext(object):
@@ -53,7 +93,7 @@ class ModulesContext(object):
         defaults = get_defaults()
         ctxt = {'arch': platform.processor(),
                 'cpuVendor': vendor,
-                'desktop_enable': defaults.get('os_desktop_enable', False)}
+                'desktop_enable': defaults.get('desktop_enable', False)}
 
         return ctxt
 
@@ -63,19 +103,21 @@ class LoginContext(object):
     def __call__(self):
         defaults = get_defaults()
         ctxt = {'additional_user_paths':
-                defaults.get('os_env_extra_user_paths'),
-                'umask': defaults.get('os_env_umask'),
-                'pwd_max_age': defaults.get('os_auth_pw_max_age'),
-                'pwd_min_age': defaults.get('os_auth_pw_min_age'),
-                'uid_min': defaults.get('os_auth_uid_min'),
-                'sys_uid_min': defaults.get('os_auth_sys_uid_min'),
-                'sys_uid_max': defaults.get('os_auth_sys_uid_max'),
-                'gid_min': defaults.get('os_auth_gid_min'),
-                'sys_gid_min': defaults.get('os_auth_sys_gid_min'),
-                'sys_gid_max': defaults.get('os_auth_sys_gid_max'),
-                'login_retries': defaults.get('os_auth_retries'),
-                'login_timeout': defaults.get('os_auth_timeout'),
-                'chfn_restrict': defaults.get('os_chfn_restrict'),
+                defaults.get('env_extra_user_paths'),
+                'umask': defaults.get('env_umask'),
+                'pwd_max_age': defaults.get('auth_pw_max_age'),
+                'pwd_min_age': defaults.get('auth_pw_min_age'),
+                'uid_min': defaults.get('auth_uid_min'),
+                'sys_uid_min': defaults.get('auth_sys_uid_min'),
+                'sys_uid_max': defaults.get('auth_sys_uid_max'),
+                'gid_min': defaults.get('auth_gid_min'),
+                'sys_gid_min': defaults.get('auth_sys_gid_min'),
+                'sys_gid_max': defaults.get('auth_sys_gid_max'),
+                'login_retries': defaults.get('auth_retries'),
+                'login_timeout': defaults.get('auth_timeout'),
+                'chfn_restrict': defaults.get('chfn_restrict'),
+                'allow_login_without_home':
+                defaults.get('auth_allow_homeless')
                 }
 
         return ctxt
@@ -84,7 +126,6 @@ class LoginContext(object):
 class ProfileContext(object):
 
     def __call__(self):
-        defaults = get_defaults()
         ctxt = {}
         return ctxt
 
@@ -93,7 +134,7 @@ class SecureTTYContext(object):
 
     def __call__(self):
         defaults = get_defaults()
-        ctxt = {'ttys': defaults.get('os_auth_root_ttys')}
+        ctxt = {'ttys': defaults.get('auth_root_ttys')}
         return ctxt
 
 
@@ -101,11 +142,12 @@ class SecurityLimitsContext(object):
 
     def __call__(self):
         defaults = get_defaults()
-        ctxt = {}
+        ctxt = {'disable_core_dump':
+                not defaults.get('enable_core_dump', False)}
         return ctxt
 
 
-def register_os_configs():
+def register_configs():
     configs = templating.HardeningConfigRenderer(templates_dir=OS_TEMPLATES)
 
     confs = {'/etc/modules':
@@ -116,15 +158,46 @@ def register_os_configs():
                              (ensure_permissions,
                               ('/etc/modules', 'root', 0o0440), {})]},
              '/etc/login.defs':
-             {'contexts': [LoginContext()]},
-             '/etc/profile.d/profile.conf':
-             {'contexts': [ProfileContext()]},
+             {'contexts': [LoginContext()],
+              'post-hooks': [(ensure_permissions,
+                              ('/etc/login.defs', 'root', 0o0444), {})]
+              },
+             '/etc/profile.d/pinerolo_profile.sh':
+             {'contexts': [ProfileContext()],
+              'post-hooks': [(ensure_permissions,
+                              ('/etc/profile.d/pinerolo_profile.sh', 0o0755,
+                               'root'), {})]
+              },
              '/etc/securetty':
-             {'contexts': [SecureTTYContext()]},
-             '/etc/security/limits.conf':
-             {'contexts': [SecurityLimitsContext()]},
+             {'contexts': [SecureTTYContext()],
+              'post-hooks': [(ensure_permissions,
+                              ('/etc/securetty', 0o0400, 'root'), {})]
+              },
              '/etc/security/limits.d/10.hardcore.conf':
-             {'contexts': [SecurityLimitsContext()]}}
+             {'contexts': [SecurityLimitsContext()],
+              'post-hooks': [(ensure_permissions,
+                              ('/etc/security/limits.d', 'root', 0o0755), {}),
+                             (ensure_permissions,
+                              ('/etc/security/limits.d/10.hardcore.conf',
+                               'root', 0o0440), {})]},
+             '/usr/share/pam-configs/tally2':
+             {'contexts': [PAMContext('tally2')],
+              'extra_files': '/usr/share/pam-configs/passwdqc',
+              'service_actions': [],
+              'post-hooks': [(ensure_permissions,
+                              ('/usr/share/pam-configs/tally2', 'root',
+                               0o0640), {}),
+                             (subprocess.check_output,
+                              (['pam-auth-update', '--package']))]},
+             '/usr/share/pam-configs/passwdqc':
+             {'contexts': [PAMContext('passwdqc')],
+              'service_actions': [],
+              'post-hooks': [(ensure_permissions,
+                              ('/usr/share/pam-configs/passwdqc', 'root',
+                               0o0640), {}),
+                             (subprocess.check_output,
+                              (['pam-auth-update', '--package']))]}
+             }
 
     for conf in confs:
         configs.register(conf, confs[conf])
@@ -133,9 +206,9 @@ def register_os_configs():
 
 
 # Run on import
-#if config('harden'):
-OS_CONFIGS = register_os_configs()
-OS_CONFIGS.write_all()
+if config('harden'):
+    OS_CONFIGS = register_configs()
+    OS_CONFIGS.write_all()
 
 
 def harden_os(f):
