6d721e2ccc1777390c0fd4f31bc8b91e7b5802bf
Edward Hope-Morley <edward.hope-morley@canonical.com>
2016-02-24 14:30:21 -0500

    more

--- tests/contrib/hardening/test_os_hardening.py
+++ tests/contrib/hardening/test_os_hardening.py
@@ -41,48 +41,60 @@ class OSHardeningTestCase(TestCase):
                     r = configs.render(k)
                     fd.write(r.encode('utf-8').strip())
 
-    def test_sysctl(self):
+    @patch.object(sysctl.os.path, 'exists')
+    @patch.object(sysctl, 'log', lambda *args, **kwargs: None)
+    def test_sysctl(self, mock_exists):
+        mock_exists.return_value = True
         ctxt = sysctl.SysCtlHardeningContext()()
+        expected = {'sysctl':
+                    {'fs.suid_dumpable': '0',
+                     'kernel.modules_disabled': '0',
+                     'kernel.randomize_va_space': '2',
+                     'kernel.sysrq': '0',
+                     'net.ipv4.conf.all.accept_redirects': '0',
+                     'net.ipv4.conf.all.accept_source_route': '0',
+                     'net.ipv4.conf.all.arp_announce': '0',
+                     'net.ipv4.conf.all.arp_ignore': '1',
+                     'net.ipv4.conf.all.log_martians': '0',
+                     'net.ipv4.conf.all.rp_filter': '1',
+                     'net.ipv4.conf.all.secure_redirects': '0',
+                     'net.ipv4.conf.all.send_redirects': '0',
+                     'net.ipv4.conf.all.shared_media': '1',
+                     'net.ipv4.conf.default.accept_redirects': '0',
+                     'net.ipv4.conf.default.accept_source_route': '0',
+                     'net.ipv4.conf.default.rp_filter': '1',
+                     'net.ipv4.conf.default.secure_redirects': '0',
+                     'net.ipv4.conf.default.send_redirects': '0',
+                     'net.ipv4.conf.default.shared_media': '1',
+                     'net.ipv4.icmp_echo_ignore_broadcasts': '1',
+                     'net.ipv4.icmp_ignore_bogus_error_responses': '1',
+                     'net.ipv4.icmp_ratelimit': '100',
+                     'net.ipv4.icmp_ratemask': '88089',
+                     'net.ipv4.ip_forward': '0',
+                     'net.ipv4.tcp_rfc1337': '1',
+                     'net.ipv4.tcp_syncookies': '1',
+                     'net.ipv4.tcp_timestamps': '0',
+                     'net.ipv6.conf.all.accept_ra': '0',
+                     'net.ipv6.conf.all.accept_redirects': '0',
+                     'net.ipv6.conf.all.disable_ipv6': '1',
+                     'net.ipv6.conf.all.forwarding': '0',
+                     'net.ipv6.conf.default.accept_ra': '0',
+                     'net.ipv6.conf.default.accept_ra_defrtr': '0',
+                     'net.ipv6.conf.default.accept_ra_pinfo': '0',
+                     'net.ipv6.conf.default.accept_ra_rtr_pref': '0',
+                     'net.ipv6.conf.default.accept_redirects': '0',
+                     'net.ipv6.conf.default.autoconf': '0',
+                     'net.ipv6.conf.default.dad_transmits': '0',
+                     'net.ipv6.conf.default.max_addresses': '1',
+                     'net.ipv6.conf.default.router_solicitations': '0'}}
 
-        expected = {'fs.suid_dumpable': '0',
-                    'kernel.modules_disabled': '0',
-                    'kernel.randomize_va_space': '2',
-                    'kernel.sysrq': '0',
-                    'net.ipv4.conf.all.accept_redirects': '0',
-                    'net.ipv4.conf.all.accept_source_route': '0',
-                    'net.ipv4.conf.all.arp_announce': '0',
-                    'net.ipv4.conf.all.arp_ignore': '1',
-                    'net.ipv4.conf.all.log_martians': '0',
-                    'net.ipv4.conf.all.rp_filter': '1',
-                    'net.ipv4.conf.all.secure_redirects': '0',
-                    'net.ipv4.conf.all.send_redirects': '0',
-                    'net.ipv4.conf.all.shared_media': '1',
-                    'net.ipv4.conf.default.accept_redirects': '0',
-                    'net.ipv4.conf.default.accept_source_route': '0',
-                    'net.ipv4.conf.default.rp_filter': '1',
-                    'net.ipv4.conf.default.secure_redirects': '0',
-                    'net.ipv4.conf.default.send_redirects': '0',
-                    'net.ipv4.conf.default.shared_media': '1',
-                    'net.ipv4.icmp_echo_ignore_broadcasts': '1',
-                    'net.ipv4.icmp_ignore_bogus_error_responses': '1',
-                    'net.ipv4.icmp_ratelimit': '100',
-                    'net.ipv4.icmp_ratemask': '88089',
-                    'net.ipv4.ip_forward': '0',
-                    'net.ipv4.tcp_rfc1337': '1',
-                    'net.ipv4.tcp_syncookies': '1',
-                    'net.ipv4.tcp_timestamps': '0',
-                    'net.ipv6.conf.all.accept_ra': '0',
-                    'net.ipv6.conf.all.accept_redirects': '0',
-                    'net.ipv6.conf.all.disable_ipv6': '1',
-                    'net.ipv6.conf.all.forwarding': '0',
-                    'net.ipv6.conf.default.accept_ra': '0',
-                    'net.ipv6.conf.default.accept_ra_defrtr': '0',
-                    'net.ipv6.conf.default.accept_ra_pinfo': '0',
-                    'net.ipv6.conf.default.accept_ra_rtr_pref': '0',
-                    'net.ipv6.conf.default.accept_redirects': '0',
-                    'net.ipv6.conf.default.autoconf': '0',
-                    'net.ipv6.conf.default.dad_transmits': '0',
-                    'net.ipv6.conf.default.max_addresses': '1',
-                    'net.ipv6.conf.default.router_solicitations': '0'}
+        self.assertEqual(ctxt, expected)
 
+    @patch.object(sysctl.os.path, 'exists')
+    @patch.object(sysctl, 'log', lambda *args, **kwargs: None)
+    def test_sysctl_enoxist(self, mock_exists):
+        mock_exists.return_value = False
+        ctxt = sysctl.SysCtlHardeningContext()()
+        expected = {'sysctl': {}}
+        self.assertTrue(mock_exists.called)
         self.assertEqual(ctxt, expected)
