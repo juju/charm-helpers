8b9b28435dab1c7c63bedd0c2735515dfd4183a2
Edward Hope-Morley <edward.hope-morley@canonical.com>
2016-02-24 15:02:35 -0500

    more

--- charmhelpers/contrib/hardening/os_hardening/checks/__init__.py
+++ charmhelpers/contrib/hardening/os_hardening/checks/__init__.py
@@ -14,6 +14,10 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with charm-helpers.  If not, see <http://www.gnu.org/licenses/>.
 
+from charmhelpers.contrib.hardening.os_hardening.checks import minimize_access
 
 def run_checks():
-    pass
+    checks = [minimize_access.NoWritePermsForPathFolders()]
+    for check in checks:
+        if not check.is_compliant():
+            check.comply()
