2dfd2b08c9b950c794fa3b6757389c896cda9362
Edward Hope-Morley <edward.hope-morley@canonical.com>
2016-03-10 13:31:20 +0000

    added apache hardening

---
+++ charmhelpers/contrib/hardening/apache/checks/config.py
@@ -0,0 +1,103 @@
+# Copyright 2016 Canonical Limited.
+#
+# This file is part of charm-helpers.
+#
+# charm-helpers is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# charm-helpers is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with charm-helpers.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import re
+import subprocess
+
+from charmhelpers.core.hookenv import (
+    log,
+    WARNING,
+)
+from charmhelpers.contrib.hardening.audits.file import (
+    FilePermissionAudit,
+    DirectoryPermissionAudit,
+    TemplatedFile,
+)
+from charmhelpers.contrib.hardening.apache import TEMPLATES_DIR
+from charmhelpers.contrib.hardening import utils
+
+
+def get_audits():
+    """Returns the audits which are used for apache.."""
+    if subprocess.call(['which', 'apache2'], stdout=subprocess.PIPE) != 0:
+        log("Apache server does not appear to be installed on this node - "
+            "skipping apache hardening", level=WARNING)
+        return
+
+    settings = utils.get_settings('apache')
+    audits = [
+        FilePermissionAudit(paths=['apache2.conf'], user='root',
+                            group='root', mode=0o0640),
+
+        TemplatedFile(os.path.join(settings['common']['apache_dir'],
+                                   'mods-available/alias.conf'),
+                      ApacheConfContext(),
+                      TEMPLATES_DIR,
+                      mode=0o0755,
+                      user='apache',
+                      service_actions=[{'service': 'apache2',
+                                        'action': ['restart']}]),
+
+        TemplatedFile(os.path.join(settings['common']['apache_dir'],
+                                   'conf-enabled/hardening.conf'),
+                      ApacheConfContext(),
+                      TEMPLATES_DIR,
+                      mode=0o0640,
+                      user='apache',
+                      service_actions=[{'service': 'apache2',
+                                        'action': ['restart']}]),
+
+        DirectoryPermissionAudit(settings['common']['apache_dir'],
+                                 user='root',
+                                 group='root',
+                                 mode=0o640)
+    ]
+
+    return audits
+
+
+class ApacheConfContext(object):
+    """Defines the set of key/value pairs to set in a apache config file.
+
+    This context, when called, will return a dictionary containing the
+    key/value pairs of setting to specify in the
+    /etc/apache/conf-enabled/hardening.conf file.
+    """
+    def __call__(self):
+        settings = utils.get_settings('apache')
+        ctxt = settings['hardening']
+
+        # change all the other files not defined as resources
+        # remove world readable files
+        cmd = ['find', settings['common']['apache_dir'], '-perm', '-o+r',
+               '-type', 'f', '-o', '-perm', '-o+w', '-type', 'f']
+        out = subprocess.check_output(cmd)
+        if out:
+            out = [o for o in out.split('\n') if o]
+            if len(out) > 0:
+                cmd = ['chmod', '-R', 'o-rw', settings['common']['apache_dir']]
+                subprocess.check_output(cmd)
+
+        for mod in settings['hardening']['modules_to_disable']:
+            subprocess.check_call(['a2dismod', mod])
+
+        out = subprocess.check_output(['apache2', '-v'])
+        ctxt['apache_version'] = re.search(r'.+version: Apache/(.+?)\s.+',
+                                           out).group(1)
+        ctxt['apache_icondir'] = '/usr/share/apache2/icons/'
+        ctxt['traceenable'] = settings['hardening']['traceenable']
+        return ctxt
