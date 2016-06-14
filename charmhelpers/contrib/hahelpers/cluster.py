8796983fdb212a9d2bc7b2e2544cf86e275da703
David Ames <david.ames@canonical.com>
2016-06-14 07:50:44 -0700

    Use XOR for vip or dns but not both

--- charmhelpers/contrib/hahelpers/cluster.py
+++ charmhelpers/contrib/hahelpers/cluster.py
@@ -311,7 +311,7 @@ def valid_hacluster_config():
     '''
     vip = config_get('vip')
     dns = config_get('dns-ha')
-    if (vip and dns) or not (vip or dns):
+    if not(bool(vip) ^ bool(dns)):
         status_set('blocked', 'HA: Either vip or dns-ha must be set but not '
                               'both in order to use high availability')
         return False
