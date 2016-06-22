6ff1a5f3f058dd9f4d8cdd1cbe6220e15840cd85
James Page <james.page@ubuntu.com>
2016-06-22 17:13:32 +0100

    Drop use of precise nrpe charm; turns out it does not have the required interfaces to support testing

--- charmhelpers/contrib/openstack/amulet/deployment.py
+++ charmhelpers/contrib/openstack/amulet/deployment.py
@@ -73,7 +73,7 @@ class OpenStackAmuletDeployment(AmuletDeployment):
         base_charms = {
             'mysql': ['precise', 'trusty'],
             'mongodb': ['precise', 'trusty'],
-            'nrpe': ['precise', 'trusty'],
+            'nrpe': ['trusty'],
         }
 
         for svc in other_services:
