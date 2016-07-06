866cf0d89895961ed352520ab7fbd2e690b50c1d
Marco Ceppi <marco@T430>
2016-07-06 10:51:33 -0400

    flake8 fixes

--- charmhelpers/contrib/openstack/exceptions.py
+++ charmhelpers/contrib/openstack/exceptions.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 class OSContextError(Exception):
     """Raised when an error occurs during context generation.
 
