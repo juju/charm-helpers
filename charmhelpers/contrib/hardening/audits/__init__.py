27a63dd7f6dcdaefe66568f916a1f1b6e3d06ec6
Marco Ceppi <marco@T430>
2016-07-06 10:51:33 -0400

    flake8 fixes

--- charmhelpers/contrib/hardening/audits/__init__.py
+++ charmhelpers/contrib/hardening/audits/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 class BaseAudit(object):  # NO-QA
     """Base class for hardening checks.
 
