f4a514a820cf91b7584358c25f6967dd94fb22d0
Christopher Glass <christopher.glass@canonical.com>
2017-01-25 17:13:37 +0000

    Fix lint error. Tests run and pass again.

--- charmhelpers/contrib/network/ip.py
+++ charmhelpers/contrib/network/ip.py
@@ -426,7 +426,7 @@ def ns_query(address):
 
     try:
         answers = dns.resolver.query(address, rtype)
-    except dns.resolver.NXDOMAIN as e:
+    except dns.resolver.NXDOMAIN:
         return None
 
     if answers:
