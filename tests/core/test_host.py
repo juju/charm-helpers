eb531bee8e1de98584bdf4d30864f702cb71a766
billy.olsen@canonical.com <>
2017-02-06 19:37:22 -0700

    [billy-olsen,r=] Allow passing of parameters to upstart scripts.
    Some upstart scripts require that additional key/value parameters
    are specified in order to narrow down the instance of the service
    which should be started/stopped/restarted/etc. This change adds
    kwargs parameters to each of the service management functions in
    order provide allow any attribute required by the underlying upstart
    script to be specified.
    
    A notable limitation is that the service_pause and service_resume
    will now allow the passing of parameters to the underlying service
    for stopping and starting the service instance. However, all of the
    service instances will be manually disabled for non-systemd init
    systems.

--- tests/core/test_host.py
+++ tests/core/test_host.py
@@ -144,6 +144,14 @@ class HelpersTest(TestCase):
         service.assert_called_with('start', service_name)
 
     @patch.object(host, 'service')
+    def test_starts_a_service_with_parms(self, service):
+        service_name = 'foo-service'
+        service.side_effect = [True]
+        self.assertTrue(host.service_start(service_name, id=4))
+
+        service.assert_called_with('start', service_name, id=4)
+
+    @patch.object(host, 'service')
     def test_stops_a_service(self, service):
         service_name = 'foo-service'
         service.side_effect = [True]
@@ -537,6 +545,30 @@ class HelpersTest(TestCase):
         os.path.exists.side_effect = [False, True]
         self.assertFalse(host.service_running('keystone'))
         service.assert_called_with('status', 'keystone')
+
+    @patch('subprocess.call')
+    @patch.object(host, 'init_is_systemd')
+    def test_service_start_with_params(self, systemd, call):
+        systemd.return_value = False
+        call.return_value = 0
+        self.assertTrue(host.service_start('ceph-osd', id=4))
+        call.assert_called_with(['service', 'ceph-osd', 'start', 'id=4'])
+
+    @patch('subprocess.call')
+    @patch.object(host, 'init_is_systemd')
+    def test_service_stop_with_params(self, systemd, call):
+        systemd.return_value = False
+        call.return_value = 0
+        self.assertTrue(host.service_stop('ceph-osd', id=4))
+        call.assert_called_with(['service', 'ceph-osd', 'stop', 'id=4'])
+
+    @patch('subprocess.call')
+    @patch.object(host, 'init_is_systemd')
+    def test_service_start_systemd_with_params(self, systemd, call):
+        systemd.return_value = True
+        call.return_value = 0
+        self.assertTrue(host.service_start('ceph-osd', id=4))
+        call.assert_called_with(['systemctl', 'start', 'ceph-osd'])
 
     @patch('grp.getgrnam')
     @patch('pwd.getpwnam')
