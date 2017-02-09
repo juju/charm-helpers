69fb73d860d2556e463fd72f794150e4c945b194
Stuart Bishop <stuart.bishop@canonical.com>
2017-02-09 20:34:24 +0700

    Handle platform.linux_platform() reporting "debian"

--- charmhelpers/osplatform.py
+++ charmhelpers/osplatform.py
@@ -8,12 +8,18 @@ def get_platform():
     will be returned (which is the name of the module).
     This string is used to decide which platform module should be imported.
     """
+    # linux_distribution is deprecated and will be removed in Python 3.7
+    # Warings *not* disabled, as we certainly need to fix this.
     tuple_platform = platform.linux_distribution()
     current_platform = tuple_platform[0]
     if "Ubuntu" in current_platform:
         return "ubuntu"
     elif "CentOS" in current_platform:
         return "centos"
+    elif "debian" in current_platform:
+        # Stock Python does not detect Ubuntu and instead returns debian.
+        # Or at least it does in some build environments like Travis CI
+        return "ubuntu"
     else:
         raise RuntimeError("This module is not supported on {}."
                            .format(current_platform))
