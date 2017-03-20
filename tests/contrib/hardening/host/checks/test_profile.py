682a99c29538c7c9290b2610cfa39f46737ea6bd
vincenzo di somma <vincenzo.di.somma@canonical.com>
2017-03-20 18:23:04 +0100

    Fixed broken test

--- tests/contrib/hardening/host/checks/test_profile.py
+++ tests/contrib/hardening/host/checks/test_profile.py
@@ -22,14 +22,14 @@ from charmhelpers.contrib.hardening.host.checks import profile
 class ProfileTestCase(TestCase):
 
     @patch.object(profile.utils, 'get_settings', lambda x:
-                  {'security': {'kernel_enable_core_dump': False}})
+                  {'security': {'kernel_enable_core_dump': False, 'ssh_tmout': 0}})
     def test_core_dump_disabled(self):
         audits = profile.get_audits()
         self.assertEqual(1, len(audits))
         self.assertTrue(isinstance(audits[0], profile.TemplatedFile))
 
     @patch.object(profile.utils, 'get_settings', lambda x: {
-        'security': {'kernel_enable_core_dump': True}
+        'security': {'kernel_enable_core_dump': True, 'ssh_tmout': 0}
     })
     def test_core_dump_enabled(self):
         audits = profile.get_audits()
