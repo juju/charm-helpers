911f583bc888a3506c0f9a4f9ad4f13a8cd2a80e
Liam Young <liam.young@canonical.com>
2017-10-17 15:27:43 +0000

    Ensure that unison users do not password expire
    Ensure that the user that is setup to use unison to sync data
    does not have an expiring password.
    
    Partial-Bug: #1686085

--- tests/contrib/unison/test_unison.py
+++ tests/contrib/unison/test_unison.py
@@ -194,10 +194,12 @@ class UnisonHelperTests(TestCase):
             for k in keys:
                 self.assertIn(call('%s\n' % k), _file.write.call_args_list)
 
+    @patch.object(unison, 'remove_password_expiry')
     @patch.object(unison, 'pwgen')
     @patch.object(unison, 'add_user_to_group')
     @patch.object(unison, 'adduser')
-    def test_ensure_user(self, adduser, to_group, pwgen):
+    def test_ensure_user(self, adduser, to_group, pwgen,
+                         remove_password_expiry):
         pwgen.return_value = sentinel.password
         unison.ensure_user('foo', group='foobar')
         adduser.assert_called_with('foo', sentinel.password)
