934d3fac5f4f13d174087b3669364ce8abd7a8c8
Frode Nordahl <frode.nordahl@gmail.com>
2018-04-20 12:15:00 +0200

    Cache config-get data (#147)
    The config() function is currently decorated with the @cached
    decorator, however, in a typical charm this will not have
    much effect as the charm will make every call to config()
    with different arguments, subsequently bypassing the cache.
    
    This commit caches the output from config-get --all and
    returns the Config object or each individual value the
    caller might ask for.

--- charmhelpers/core/hookenv.py
+++ charmhelpers/core/hookenv.py
@@ -354,21 +354,30 @@ class Config(dict):
             self.save()
 
 
-@cached
+_cache_config = None
+
+
 def config(scope=None):
-    """Juju charm configuration"""
-    config_cmd_line = ['config-get']
-    if scope is not None:
-        config_cmd_line.append(scope)
-    else:
-        config_cmd_line.append('--all')
-    config_cmd_line.append('--format=json')
+    """
+    Get the juju charm configuration (scope==None) or individual key,
+    (scope=str).  The returned value is a Python data structure loaded as
+    JSON from the Juju config command.
+
+    :param scope: If set, return the value for the specified key.
+    :type scope: Optional[str]
+    :returns: Either the whole config as a Config, or a key from it.
+    :rtype: Any
+    """
+    global _cache_config
+    config_cmd_line = ['config-get', '--all', '--format=json']
     try:
-        config_data = json.loads(
-            subprocess.check_output(config_cmd_line).decode('UTF-8'))
+        if _cache_config is None:
+            config_data = json.loads(
+                subprocess.check_output(config_cmd_line).decode('UTF-8'))
+            _cache_config = Config(config_data)
         if scope is not None:
-            return config_data
-        return Config(config_data)
+            return _cache_config.get(scope)
+        return _cache_config
     except ValueError:
         return None
 
