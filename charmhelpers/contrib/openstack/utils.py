b5c03907830d6528cff504d3c522e053b3d17e3c
David Ames <david@davam.net>
2018-09-14 02:08:40 -0700

    Add helpers for common series upgrade tasks (#212)
    For many of the OpenStack API charms the tasks for series upgrade
    prepare and complete are the same between charms. These helpers run
    those common tasks.

--- charmhelpers/contrib/openstack/utils.py
+++ charmhelpers/contrib/openstack/utils.py
@@ -1733,3 +1733,19 @@ def is_unit_upgrading_set():
             return not(not(kv.get('unit-upgrading')))
     except Exception:
         return False
+
+
+def series_upgrade_prepare(pause_unit_helper=None, configs=None):
+    """ Run common series upgrade prepare tasks."""
+    set_unit_upgrading()
+    if pause_unit_helper and configs:
+        if not is_unit_paused_set():
+            pause_unit_helper(configs)
+
+
+def series_upgrade_complete(resume_unit_helper=None, configs=None):
+    """ Run common series upgrade complete tasks."""
+    clear_unit_paused()
+    clear_unit_upgrading()
+    if resume_unit_helper and configs:
+        resume_unit_helper(configs)
