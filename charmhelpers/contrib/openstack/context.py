d5a43dd3c0db86b3828582a0837ce6b8a5d78338
Vladimir Grevtsev <vlgrevtsev@gmail.com>
2018-09-18 16:07:20 +0200

    Remove unnecessary charm relation option (#219)

--- charmhelpers/contrib/openstack/context.py
+++ charmhelpers/contrib/openstack/context.py
@@ -1523,10 +1523,6 @@ class NeutronAPIContext(OSContextGenerator):
                 'rel_key': 'enable-nsg-logging',
                 'default': False,
             },
-            'nsg_log_output_base': {
-                'rel_key': 'nsg-log-output-base',
-                'default': None,
-            },
         }
         ctxt = self.get_neutron_options({})
         for rid in relation_ids('neutron-plugin-api'):
