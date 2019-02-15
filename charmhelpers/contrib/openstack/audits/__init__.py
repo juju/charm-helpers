9768c74e7dcfdecb0802a42e89a9e2e85093f2ba
Chris MacNaughton <chris.macnaughton@canonical.com>
2019-02-15 15:21:48 +0100

    fix shadowing flake8 error

--- charmhelpers/contrib/openstack/audits/__init__.py
+++ charmhelpers/contrib/openstack/audits/__init__.py
@@ -51,7 +51,7 @@ def audit(*args):
             raise RuntimeError(
                 "Test name '{}' used more than once"
                 .format(test_name))
-        non_callables = [f for f in args if not callable(f)]
+        non_callables = [fn for fn in args if not callable(fn)]
         if non_callables:
             raise RuntimeError("Configuration includes non-callable filters: {}"
                                .format(non_callables))
