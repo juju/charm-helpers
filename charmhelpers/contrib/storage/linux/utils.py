8e5e48dee41c26d73f07ebd2df5892898001eef7
Tytus Kurek <tytus.kurek@canonical.com>
2019-04-26 15:00:21 +0000

    Make XFS inode size configurable
    Make XFS inode size configurable, so that charm could set it to the
    desired value, if different than 1024.
    
    Partial-Bug: 1826552

--- charmhelpers/contrib/storage/linux/utils.py
+++ charmhelpers/contrib/storage/linux/utils.py
@@ -110,17 +110,19 @@ def is_device_mounted(device):
     return bool(re.search(r'MOUNTPOINT=".+"', out))
 
 
-def mkfs_xfs(device, force=False):
+def mkfs_xfs(device, force=False, inode_size=1024):
     """Format device with XFS filesystem.
 
     By default this should fail if the device already has a filesystem on it.
     :param device: Full path to device to format
     :ptype device: tr
     :param force: Force operation
-    :ptype: force: boolean"""
+    :ptype: force: boolean
+    :param inode_size: XFS inode size in bytes
+    :ptype inode_size: int"""
     cmd = ['mkfs.xfs']
     if force:
         cmd.append("-f")
 
-    cmd += ['-i', 'size=1024', device]
+    cmd += ['-i', "size={}".format(inode_size), device]
     check_call(cmd)
