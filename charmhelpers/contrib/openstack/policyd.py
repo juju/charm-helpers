ea60159e1e657a4845b816da4c772a2cb43d59c3
Alex Kavanagh <alex@ajkavanagh.co.uk>
2019-11-21 13:06:52 +0000

    Fix grammar/word nit

--- charmhelpers/contrib/openstack/policyd.py
+++ charmhelpers/contrib/openstack/policyd.py
@@ -685,9 +685,9 @@ def process_policy_resource_file(resource_file,
     black list.
 
     The yaml filename can be modified in two ways.  If the `preserve_topdir`
-    param is True, then files will be flattened to the this top dir.  This
-    allows for creating sets of files that can be grouped into a single level
-    tree structure.
+    param is True, then files will be flattened to the top dir.  This allows
+    for creating sets of files that can be grouped into a single level tree
+    structure.
 
     Secondly, if the `preprocess_filename` param is not None and callable()
     then the name is passed to that function for preprocessing before being
