868d2ef2755f57d4ca0c0e6de95b8fe573dec3c1
Adam Dyess <adam.dyess@canonical.com>
2022-04-28 10:46:45 -0500

    ArchiveUrlFetchHandler.download(...) needs to be able to access proxy variables (#700)
    * fetch.archiveurl needs to use available proxies during download(...)
    
    * call ProxyHandler() and urlopen(...) with an updated os.environ to make use of no_proxy
    
    * update os.environ rather than overwriting it

--- charmhelpers/fetch/archiveurl.py
+++ charmhelpers/fetch/archiveurl.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import contextlib
 import os
 import hashlib
 import re
@@ -24,11 +25,15 @@ from charmhelpers.payload.archive import (
     get_archive_handler,
     extract,
 )
+from charmhelpers.core.hookenv import (
+    env_proxy_settings,
+)
 from charmhelpers.core.host import mkdir, check_hash
 
 from urllib.request import (
     build_opener, install_opener, urlopen, urlretrieve,
     HTTPPasswordMgrWithDefaultRealm, HTTPBasicAuthHandler,
+    ProxyHandler
 )
 from urllib.parse import urlparse, urlunparse, parse_qs
 from urllib.error import URLError
@@ -48,6 +53,20 @@ def splitpasswd(user):
     if match:
         return match.group(1, 2)
     return user, None
+
+
+@contextlib.contextmanager
+def proxy_env():
+    """
+    Creates a context which temporarily modifies the proxy settings in os.environ.
+    """
+    restore = {**os.environ}  # Copy the current os.environ
+    juju_proxies = env_proxy_settings() or {}
+    os.environ.update(**juju_proxies)  # Insert or Update the os.environ
+    yield os.environ
+    for key in juju_proxies:
+        del os.environ[key]  # remove any keys which were added or updated
+    os.environ.update(**restore)  # restore any original values
 
 
 class ArchiveUrlFetchHandler(BaseFetchHandler):
@@ -80,6 +99,7 @@ class ArchiveUrlFetchHandler(BaseFetchHandler):
         # propagate all exceptions
         # URLError, OSError, etc
         proto, netloc, path, params, query, fragment = urlparse(source)
+        handlers = []
         if proto in ('http', 'https'):
             auth, barehost = splituser(netloc)
             if auth is not None:
@@ -89,10 +109,13 @@ class ArchiveUrlFetchHandler(BaseFetchHandler):
                 # Realm is set to None in add_password to force the username and password
                 # to be used whatever the realm
                 passman.add_password(None, source, username, password)
-                authhandler = HTTPBasicAuthHandler(passman)
-                opener = build_opener(authhandler)
-                install_opener(opener)
-        response = urlopen(source)
+                handlers.append(HTTPBasicAuthHandler(passman))
+
+        with proxy_env():
+            handlers.append(ProxyHandler())
+            opener = build_opener(*handlers)
+            install_opener(opener)
+            response = urlopen(source)
         try:
             with open(dest, 'wb') as dest_file:
                 dest_file.write(response.read())
