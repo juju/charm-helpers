b4e5b61f820f02a70f47c7abc68a96fb4f689a73
fdesi <francesco.desimone@canonical.com>
2022-06-24 08:46:16 +0200

    remove useless check

--- charmhelpers/contrib/storage/linux/utils.py
+++ charmhelpers/contrib/storage/linux/utils.py
@@ -15,10 +15,6 @@
 import os
 import re
 from stat import S_ISBLK
-from charmhelpers.core.hookenv import (
-    log,
-    WARNING
-)
 
 from subprocess import (
     CalledProcessError,
@@ -132,8 +128,6 @@ def mkfs_xfs(device, force=False, inode_size=None):
     if inode_size:
         if inode_size >= 256 and inode_size <= 2048:
             cmd += ['-i', "size={}".format(inode_size)]
-        elif inode_size != 0:
-            log("Config value xfs-inode-size={} is invalid. Using system default.".format(inode_size), level=WARNING)
 
     cmd += [device]
     check_call(cmd)
