32772ff502e179027f46daaa04729a2f5d49f5e5
Felipe Reyes <felipe.reyes@canonical.com>
2023-06-13 08:55:46 -0400

    Service token support [yoga] (#797)
    * Add configuration for use of service tokens
    
    Add a new template to configure the service_user ini file section and
    also add two needed parameters in keystone_authtoken configuration
    
    (cherry picked from commit f8c8861b4f0cb2181792013f9a973b3924a1e831)
    
    * Fixes for service tokens
    
    * Make admin role configurable
    * Move keystone_authtoken configuration from mitaka version to base
      version
    * Sync service_user config with keystone_authtoken for the base version
    
    (cherry picked from commit 7bcb1942ad11aafa307d5872d7b36a7bd7908288)
    
    Conflicts Resolved:
    	charmhelpers/contrib/openstack/context.py
    
    * Fix tests
    
    (cherry picked from commit 4090532d1194d215cf5b065c1c108072652b8cfe)
    
    * Add test for this feature
    
    (cherry picked from commit e43008d7671433e44e27c58312292d8fb504fc59)
    
    * Fix test
    
    (cherry picked from commit c7cc5db9e4acd4dd66282bd4303f59db9ef8e9c6)
    
    * Add service token parameters to last authtoken template
    
    Copy parameters added in pull request #740 to the newest
    keystone-authtoken template.
    
    (cherry picked from commit 23d838bc55c842ea27ba29f4b1097b2ec9469fca)
    
    * Pin tox<4.0
    
    ---------
    
    Co-authored-by: Jorge Merlino <jorge.merlino@canonical.com>

--- tests/contrib/openstack/test_os_contexts.py
+++ tests/contrib/openstack/test_os_contexts.py
@@ -217,6 +217,11 @@ IDENTITY_SERVICE_RELATION_VERSIONED = {
 }
 IDENTITY_SERVICE_RELATION_VERSIONED.update(IDENTITY_SERVICE_RELATION_HTTPS)
 
+IDENTITY_SERVICE_RELATION_ADMIN_ROLE = {
+    'admin_role': 'Role',
+}
+IDENTITY_SERVICE_RELATION_ADMIN_ROLE.update(IDENTITY_SERVICE_RELATION_HTTPS)
+
 IDENTITY_CREDENTIALS_RELATION_VERSIONED = {
     'api_version': '3',
     'service_tenant_id': 'svc-proj-id',
@@ -1022,6 +1027,7 @@ class ContextTests(unittest.TestCase):
         result = identity_service()
         expected = {
             'admin_password': 'foo',
+            'admin_role': 'Admin',
             'admin_tenant_name': 'admin',
             'admin_tenant_id': None,
             'admin_domain_id': None,
@@ -1078,6 +1084,7 @@ class ContextTests(unittest.TestCase):
         result = identity_service()
         expected = {
             'admin_password': 'foo',
+            'admin_role': 'Admin',
             'admin_tenant_name': 'admin',
             'admin_tenant_id': None,
             'admin_domain_id': None,
@@ -1109,6 +1116,7 @@ class ContextTests(unittest.TestCase):
         result = identity_service()
         expected = {
             'admin_password': 'foo',
+            'admin_role': 'Admin',
             'admin_tenant_name': 'admin',
             'admin_tenant_id': None,
             'admin_domain_id': None,
@@ -1139,6 +1147,7 @@ class ContextTests(unittest.TestCase):
         result = identity_service()
         expected = {
             'admin_password': 'foo',
+            'admin_role': 'Admin',
             'admin_tenant_name': 'admin',
             'admin_tenant_id': '123456',
             'admin_domain_id': None,
@@ -1168,6 +1177,7 @@ class ContextTests(unittest.TestCase):
         result = identity_service()
         expected = {
             'admin_password': 'foo',
+            'admin_role': 'Admin',
             'admin_tenant_name': 'admin',
             'admin_tenant_id': None,
             'admin_domain_id': None,
@@ -1198,6 +1208,7 @@ class ContextTests(unittest.TestCase):
         result = identity_service()
         expected = {
             'admin_password': 'foo',
+            'admin_role': 'Admin',
             'admin_domain_name': 'admin_domain',
             'admin_tenant_name': 'admin',
             'admin_tenant_id': 'svc-proj-id',
@@ -1216,6 +1227,37 @@ class ContextTests(unittest.TestCase):
             'internal_port': '5000',
             'internal_protocol': 'https',
             'api_version': '3',
+        }
+        result.pop('keystone_authtoken')
+        self.assertEquals(result, expected)
+
+    @patch.object(context, 'filter_installed_packages', return_value=[])
+    @patch.object(context, 'os_release', return_value='rocky')
+    def test_identity_service_context_with_admin_role(self, *args):
+        '''Test shared-db context with admin role supplied from keystone'''
+        relation = FakeRelation(
+            relation_data=IDENTITY_SERVICE_RELATION_ADMIN_ROLE)
+        self.relation_get.side_effect = relation.get
+        identity_service = context.IdentityServiceContext()
+        result = identity_service()
+        expected = {
+            'admin_password': 'foo',
+            'admin_role': 'Role',
+            'admin_tenant_name': 'admin',
+            'admin_tenant_id': None,
+            'admin_domain_id': None,
+            'admin_user': 'adam',
+            'auth_host': 'keystone-host.local',
+            'auth_port': '35357',
+            'auth_protocol': 'https',
+            'service_host': 'keystonehost.local',
+            'service_port': '5000',
+            'service_protocol': 'https',
+            'service_type': 'volume',
+            'internal_host': 'keystone-internal.local',
+            'internal_port': '5000',
+            'internal_protocol': 'https',
+            'api_version': '2.0',
         }
         result.pop('keystone_authtoken')
         self.assertEquals(result, expected)
@@ -1256,6 +1298,7 @@ class ContextTests(unittest.TestCase):
         result = identity_service()
         expected = {
             'admin_password': 'foo',
+            'admin_role': 'Admin',
             'admin_tenant_name': 'admin',
             'admin_tenant_id': '123456',
             'admin_domain_id': None,
